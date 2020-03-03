e913c972c19656baea4bb22cc150186d010198a8
Jack Baron <jackmbaron@gmail.com>
2020-03-03 09:49:46 +0000

    add game compatibility attribute

---
+++ MelonLoader.ModHandler/MelonModGameAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MelonLoader
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class MelonModGameAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the target Developer
+        /// </summary>
+        public string Developer { get; }
+
+        /// <summary>
+        /// Gets target Game Name
+        /// </summary>
+        public string GameName { get; }
+
+        /// <summary>
+        /// Gets whether this mod can target any game
+        /// </summary>
+        public bool Universal
+        {
+            get => Developer == null && GameName == null;
+        }
+
+        /// <summary>
+        /// Mark this game as compatible with any game
+        /// </summary>
+        public MelonModGameAttribute()
+        {
+            Developer = null;
+            GameName = null;
+        }
+
+        public MelonModGameAttribute(string developer, string gameName)
+        {
+            Developer = developer ?? throw new ArgumentNullException(nameof(developer));
+            GameName = gameName ?? throw new ArgumentNullException(nameof(gameName));
+        }
+    }
+}
