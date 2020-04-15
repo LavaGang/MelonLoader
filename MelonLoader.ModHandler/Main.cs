05f1b95d442af8157e3620fbffb8f705b3c38e37
Herp Derpinstine <herpderpinstine@gmail.com>
2020-04-15 03:20:36 -0500

    Updated BuildInfo Added an issue to CHANGELOG's list

--- MelonLoader.ModHandler/Main.cs
+++ MelonLoader.ModHandler/Main.cs
@@ -9,8 +9,8 @@ namespace MelonLoader
     internal static class BuildInfo
     {
         internal const string Name = "MelonLoader";
-        internal const string Author = "Herp Derpinstine";
-        internal const string Company = "NanoNuke @ nanonuke.net";
+        internal const string Author = "Lava Gang";
+        internal const string Company = "discord.gg/2Wn3N2P";
         internal const string Version = "0.0.4";
     }
 
