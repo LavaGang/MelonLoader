f3dbf489ca4a9cfa5b770a0741827919e4b4a09e
Herp Derpinstine <herpderpinstine@gmail.com>
2020-04-16 06:00:49 -0500

    Fixed issue with MelonCoroutines throwing a cast exception for knah's Generator

--- MelonLoader.ModHandler/MelonCoroutines.cs
+++ MelonLoader.ModHandler/MelonCoroutines.cs
@@ -8,25 +8,66 @@ namespace MelonLoader
 {
     public class MelonCoroutines
     {
+        private class CoroD
+        {
+            internal System.Type CoroutineType;
+            internal object Coroutine;
+            private MethodInfo MoveNextMethod;
+            private PropertyInfo CurrentProp;
+
+            internal object GetCurrent()
+            {
+                if (CurrentProp == null)
+                    CurrentProp = CoroutineType.GetProperty("Current", BindingFlags.Instance | BindingFlags.Public);
+                if (CurrentProp != null)
+                    return CurrentProp.GetGetMethod().Invoke(Coroutine, new object[] { });
+                return null;
+            }
+
+            internal bool MoveNext()
+            {
+                if (MoveNextMethod == null)
+                    MoveNextMethod = CoroutineType.GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.Public);
+                if (MoveNextMethod != null)
+                    return (bool)MoveNextMethod.Invoke(Coroutine, new object[] { });
+                return true;
+            }
+        }
+
         private struct CoroTuple
         {
             public object WaitCondition;
-            public IEnumerator Coroutine;
+            public CoroD Coroutine;
         }
         private static List<CoroTuple> ourCoroutinesStore = new List<CoroTuple>();
-        private static List<IEnumerator> ourNextFrameCoroutines = new List<IEnumerator>();
+        private static List<CoroD> ourNextFrameCoroutines = new List<CoroD>();
 
-        public static void Start(IEnumerator routine)
+        public static void Start<T>(T routine)
         {
-            if (Imports.IsIl2CppGame() && !Imports.IsMUPOTMode())
-                ProcessNextOfCoroutine(routine);
-            else
-                MelonModComponent.Instance.StartCoroutine(routine);
+            if (routine != null)
+            {
+                if (Imports.IsIl2CppGame() && !Imports.IsMUPOTMode())
+                {
+                    System.Type coroutineType = typeof(T);
+                    string asm_name = coroutineType?.Assembly?.GetName()?.Name;
+                    ProcessNextOfCoroutine(new CoroD() { CoroutineType = coroutineType, Coroutine = routine });
+                }
+                else
+                    MelonModComponent.Instance?.StartCoroutine((IEnumerator)routine);
+            }
         }
 
-        static FieldInfo field_m_Seconds = null;
+        private static bool HasCheckWaitCondition = false;
+        private static bool WaitConditionIsIl2Cpp = true;
+        private static FieldInfo field_m_Seconds = null;
+        private static PropertyInfo prop_m_Seconds = null;
         internal static void Process()
         {
+            if (HasCheckWaitCondition == false)
+            {
+                WaitConditionIsIl2Cpp = (typeof(WaitForSeconds).GetProperty("m_Seconds", BindingFlags.Instance | BindingFlags.Public) != null);
+                HasCheckWaitCondition = true;
+            }
             for (var i = ourCoroutinesStore.Count - 1; i >= 0; i--)
             {
                 var tuple = ourCoroutinesStore[i];
@@ -40,10 +81,28 @@ namespace MelonLoader
                         }
                         break;
                     case WaitForSeconds waitForSeconds:
-                        if (field_m_Seconds == null)
-                            field_m_Seconds = typeof(WaitForSeconds).GetField("m_Seconds");
-                        float m_Seconds = (float)field_m_Seconds.GetValue(waitForSeconds);
-                        field_m_Seconds.SetValue(waitForSeconds, (m_Seconds - Time.deltaTime));
+                        float m_Seconds = 0;
+
+                        if (WaitConditionIsIl2Cpp)
+                        {
+                            if (prop_m_Seconds == null)
+                                prop_m_Seconds = typeof(WaitForSeconds).GetProperty("m_Seconds");
+                            if (prop_m_Seconds != null)
+                            {
+                                m_Seconds = (float)prop_m_Seconds.GetValue(waitForSeconds);
+                                prop_m_Seconds.SetValue(waitForSeconds, (m_Seconds - Time.deltaTime));
+                            }
+                        }
+                        else
+                        {
+                            if (field_m_Seconds == null)
+                                field_m_Seconds = typeof(WaitForSeconds).GetField("m_Seconds", BindingFlags.Instance | BindingFlags.NonPublic);
+                            if (field_m_Seconds != null)
+                            {
+                               m_Seconds = (float)field_m_Seconds.GetValue(waitForSeconds);
+                               field_m_Seconds.SetValue(waitForSeconds, (m_Seconds - Time.deltaTime));
+                            }
+                        }
                         if (m_Seconds <= 0)
                         {
                             ourCoroutinesStore.RemoveAt(i);
@@ -60,7 +119,7 @@ namespace MelonLoader
                 ProcessNextOfCoroutine(nextFrameCoroutine);
         }
 
-        private static void ProcessNextOfCoroutine(IEnumerator enumerator)
+        private static void ProcessNextOfCoroutine(CoroD enumerator)
         {
             if (!enumerator.MoveNext())
             {
@@ -73,12 +132,12 @@ namespace MelonLoader
                 }
                 return;
             }
-            var next = enumerator.Current;
+            var next = enumerator.GetCurrent();
             if (next == null)
                 ourNextFrameCoroutines.Add(enumerator);
             else
             {
-                if (next is IEnumerator nextCoro)
+                if (next is CoroD nextCoro)
                     ProcessNextOfCoroutine(nextCoro);
                 ourCoroutinesStore.Add(new CoroTuple() { WaitCondition = next, Coroutine = enumerator });
             }
