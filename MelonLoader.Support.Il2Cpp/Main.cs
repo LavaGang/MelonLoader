32177336bdd925d7f7505b22278b615d44717461
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-05-29 21:30:41 +0200

    Update unhollower reference to 0.4.1, tell unhollower the unity version

--- MelonLoader.Support.Il2Cpp/Main.cs
+++ MelonLoader.Support.Il2Cpp/Main.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using UnhollowerBaseLib;
+using UnhollowerBaseLib.Runtime;
 using UnhollowerRuntimeLib;
 using UnityEngine;
 using UnityEngine.Events;
@@ -28,10 +30,22 @@ namespace MelonLoader.Support
                 LogSupport.TraceHandler += MelonModLogger.LogWarning;
             }
             ClassInjector.DoHook += Imports.Hook;
+            GetUnityVersionNumbers(out var major, out var minor, out var patch);
+            UnityVersionHandler.Initialize(major, minor, patch);
             ClassInjector.RegisterTypeInIl2Cpp<MelonLoaderComponent>();
             MelonLoaderComponent.CreateComponent();
             add_sceneLoaded_potato(new Action<Scene, LoadSceneMode>(OnSceneLoad));
             return new Module();
+        }
+
+        private static void GetUnityVersionNumbers(out int major, out int minor, out int patch)
+        {
+            var unityVersionSplit = Application.unityVersion.Split('.');
+            major = int.Parse(unityVersionSplit[0]);
+            minor = int.Parse(unityVersionSplit[1]);
+            var patchString = unityVersionSplit[2];
+            var firstBadChar = patchString.FirstOrDefault(it => it < '0' || it > '9');
+            patch = int.Parse(firstBadChar == 0 ? patchString : patchString.Substring(0, patchString.IndexOf(firstBadChar)));
         }
 
         private static void add_sceneLoaded_potato(Action<Scene, LoadSceneMode> action)
