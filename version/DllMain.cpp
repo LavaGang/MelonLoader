eff58d7363473dcc82087e2e4aaa7780279eff62
Herp Derpinstine <herpderpinstine@gmail.com>
2020-06-07 16:22:35 -0500

    Cleaned up version.dll, Made version.dll look for the original dll in the root directory as well under the name version_original.dll

--- version/DllMain.cpp
+++ version/DllMain.cpp
@@ -1,8 +1,6 @@
 #include <Windows.h>
 #include <string>
 
-static HINSTANCE thisdll;
-
 extern "C" FARPROC GetFileVersionInfoA_Original = NULL;
 extern "C" FARPROC GetFileVersionInfoByHandle_Original = NULL;
 extern "C" FARPROC GetFileVersionInfoExA_Original = NULL;
@@ -23,57 +21,56 @@ extern "C" FARPROC VerQueryValueW_Original = NULL;
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	thisdll = hinstDLL;
-	if (fdwReason == DLL_PROCESS_ATTACH)
+	if (fdwReason != DLL_PROCESS_ATTACH)
+		return TRUE;
+	LPSTR filepath = new CHAR[MAX_PATH];
+	GetModuleFileName(GetModuleHandle(NULL), filepath, MAX_PATH);
+	std::string filepathstr = filepath;
+	HINSTANCE originaldll = LoadLibrary((filepathstr.substr(0, filepathstr.find_last_of(".")) + "_original.dll").c_str());
+	if (originaldll == NULL)
 	{
-		DisableThreadLibraryCalls(thisdll);
 		char* path = new char[MAX_PATH];
-		if (GetWindowsDirectory(path, MAX_PATH) != NULL)
+		if (GetSystemDirectory(path, MAX_PATH) != NULL)
+			originaldll = LoadLibrary((std::string(path) + "\\version.dll").c_str());
+		else
 		{
-			HINSTANCE originaldll = LoadLibrary((std::string(path) + "\\System32\\version.dll").c_str());
-			if (originaldll)
-			{
-				GetFileVersionInfoA_Original = GetProcAddress(originaldll, "GetFileVersionInfoA");
-				GetFileVersionInfoByHandle_Original = GetProcAddress(originaldll, "GetFileVersionInfoByHandle");
-				GetFileVersionInfoExA_Original = GetProcAddress(originaldll, "GetFileVersionInfoExA");
-				GetFileVersionInfoExW_Original = GetProcAddress(originaldll, "GetFileVersionInfoExW");
-				GetFileVersionInfoSizeA_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeA");
-				GetFileVersionInfoSizeExA_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeExA");
-				GetFileVersionInfoSizeExW_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeExW");
-				GetFileVersionInfoSizeW_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeW");
-				GetFileVersionInfoW_Original = GetProcAddress(originaldll, "GetFileVersionInfoW");
-				VerFindFileA_Original = GetProcAddress(originaldll, "VerFindFileA");
-				VerFindFileW_Original = GetProcAddress(originaldll, "VerFindFileW");
-				VerInstallFileA_Original = GetProcAddress(originaldll, "VerInstallFileA");
-				VerInstallFileW_Original = GetProcAddress(originaldll, "VerInstallFileW");
-				VerLanguageNameA_Original = GetProcAddress(originaldll, "VerLanguageNameA");
-				VerLanguageNameW_Original = GetProcAddress(originaldll, "VerLanguageNameW");
-				VerQueryValueA_Original = GetProcAddress(originaldll, "VerQueryValueA");
-				VerQueryValueW_Original = GetProcAddress(originaldll, "VerQueryValueW");
-
-				LPSTR filepath = new CHAR[MAX_PATH];
-				GetModuleFileName(GetModuleHandle(NULL), filepath, MAX_PATH);
-				if ((strstr(filepath, "UnityCrashHandler") == NULL) && (strstr(GetCommandLine(), "--no-mods") == NULL))
-				{
-					HINSTANCE melonloaderdll = LoadLibrary("MelonLoader\\MelonLoader.dll");
-					if (melonloaderdll != NULL)
-						return TRUE;
-					else
-						MessageBox(NULL, "Failed to Load MelonLoader.dll!", "MelonLoader", MB_ICONERROR | MB_OK);
-				}
-				else
-					return TRUE;
-			}
+			MessageBox(NULL, "Failed to Get System32 Directory!", "MelonLoader", MB_ICONERROR | MB_OK);
+			return FALSE;
+		}
+	}
+	if (originaldll != NULL)
+	{
+		GetFileVersionInfoA_Original = GetProcAddress(originaldll, "GetFileVersionInfoA");
+		GetFileVersionInfoByHandle_Original = GetProcAddress(originaldll, "GetFileVersionInfoByHandle");
+		GetFileVersionInfoExA_Original = GetProcAddress(originaldll, "GetFileVersionInfoExA");
+		GetFileVersionInfoExW_Original = GetProcAddress(originaldll, "GetFileVersionInfoExW");
+		GetFileVersionInfoSizeA_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeA");
+		GetFileVersionInfoSizeExA_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeExA");
+		GetFileVersionInfoSizeExW_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeExW");
+		GetFileVersionInfoSizeW_Original = GetProcAddress(originaldll, "GetFileVersionInfoSizeW");
+		GetFileVersionInfoW_Original = GetProcAddress(originaldll, "GetFileVersionInfoW");
+		VerFindFileA_Original = GetProcAddress(originaldll, "VerFindFileA");
+		VerFindFileW_Original = GetProcAddress(originaldll, "VerFindFileW");
+		VerInstallFileA_Original = GetProcAddress(originaldll, "VerInstallFileA");
+		VerInstallFileW_Original = GetProcAddress(originaldll, "VerInstallFileW");
+		VerLanguageNameA_Original = GetProcAddress(originaldll, "VerLanguageNameA");
+		VerLanguageNameW_Original = GetProcAddress(originaldll, "VerLanguageNameW");
+		VerQueryValueA_Original = GetProcAddress(originaldll, "VerQueryValueA");
+		VerQueryValueW_Original = GetProcAddress(originaldll, "VerQueryValueW");
+		if ((strstr(filepath, "UnityCrashHandler") == NULL) && (strstr(GetCommandLine(), "--no-mods") == NULL))
+		{
+			HINSTANCE melonloaderdll = LoadLibrary("MelonLoader\\MelonLoader.dll");
+			if (melonloaderdll != NULL)
+				return TRUE;
 			else
-				MessageBox(NULL, "Failed to Load version.dll!", "MelonLoader", MB_ICONERROR | MB_OK);
+				MessageBox(NULL, "Failed to Load MelonLoader.dll!", "MelonLoader", MB_ICONERROR | MB_OK);
 		}
 		else
-			MessageBox(NULL, "Failed to Get Windows Directory!", "MelonLoader", MB_ICONERROR | MB_OK);
-		return FALSE;
+			return TRUE;
 	}
-	else if (fdwReason == DLL_PROCESS_DETACH)
-		FreeLibrary(thisdll);
-	return TRUE;
+	else
+		MessageBox(NULL, "Failed to Load version.dll!", "MelonLoader", MB_ICONERROR | MB_OK);
+	return FALSE;
 }
 
 extern "C" void GetFileVersionInfoA_ML();
