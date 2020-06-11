658de20a24a0fdbada5ed9a303fe4e5803c20459
Herp Derpinstine <herpderpinstine@gmail.com>
2020-06-11 00:21:31 -0500

    Edit Mod Settings Menu

--- MelonLoader.ModHandler/ModSettingsMenu.cs
+++ MelonLoader.ModHandler/ModSettingsMenu.cs
@@ -9,21 +9,29 @@ namespace MelonLoader.ModSettingsMenu
     public interface RenderHelper
     {
         object new_Rect(float x, float y, float width, float height);
+        object new_Vector2(float x, float y);
+        float Rect_get_width(object rect);
+        float Rect_get_height(object rect);
+        bool Input_GetKeyDown(int key);
         object GUI_Window(int id, object clientRect, Action<int> func, string title);
         bool GUI_Button(object position, string text);
-        bool Input_GetKeyDown(int key);
+        object GUI_BeginScrollView(object position, object scrollPosition, object viewRect);
+        void GUI_EndScrollView(bool handleScrollWheel = false);
     }
 
     class Main
     {
-        internal static RenderHelper renderHelper = null;
+        private static RenderHelper renderHelper = null;
         private static bool IsOpen = false;
         private static object WindowRect = null;
-        internal static object ToolbarRect = null;
+        private static object ToolbarRect = null;
+        private static object ScrollViewRect = null;
+        private static object ScrollViewVector = null;
+        private static object ScrollViewViewRect = null;
 
-        internal static Main.ToolbarPage CurrentPage = 0;
+        private static ToolbarPage CurrentPage = ToolbarPage.SETTINGS;
         private static string[] PageNames = { "ABOUT", "SETTINGS" };
-        internal enum ToolbarPage
+        private enum ToolbarPage
         {
             ABOUT = 0,
             SETTINGS = 1,
@@ -40,6 +48,9 @@ namespace MelonLoader.ModSettingsMenu
 
             WindowRect = renderHelper.new_Rect(10, 10, 400, 400);
             ToolbarRect = renderHelper.new_Rect(6, 22, 388, 25);
+            ScrollViewRect = renderHelper.new_Rect(6, 52, (renderHelper.Rect_get_width(WindowRect) - 12), (renderHelper.Rect_get_height(WindowRect) - 58));
+            ScrollViewVector = renderHelper.new_Vector2(0, 0);
+            ScrollViewViewRect = renderHelper.new_Rect(0, 0, renderHelper.Rect_get_width(ScrollViewRect), renderHelper.Rect_get_height(ScrollViewRect));
         }
 
         internal static void Render()
@@ -53,7 +64,12 @@ namespace MelonLoader.ModSettingsMenu
 
             WindowRect = renderHelper.GUI_Window(0, WindowRect, new Action<int>(x =>
             {
-                // Render Toolbar Here
+                for (int i = 0; i < PageNames.Length; i++)
+                {
+                    string page = PageNames[i];
+
+                    // Draw Buttons
+                }
 
                 switch(CurrentPage)
                 {
@@ -76,7 +92,11 @@ namespace MelonLoader.ModSettingsMenu
 
         private static void Render_Settings()
         {
+            ScrollViewVector = renderHelper.GUI_BeginScrollView(ScrollViewRect, ScrollViewVector, ScrollViewViewRect);
 
+            // Render Settings
+
+            renderHelper.GUI_EndScrollView(true);
         }
 
         private static bool IsInputPressed = false;
--- MelonLoader.Support.Il2Cpp/RenderHelper.cs
+++ MelonLoader.Support.Il2Cpp/RenderHelper.cs
@@ -9,12 +9,18 @@ namespace MelonLoader.Support
     class RenderHelper : ModSettingsMenu.RenderHelper
     {
         public object new_Rect(float x, float y, float width, float height) => new Rect(x, y, width, height);
+        public object new_Vector2(float x, float y) => new Vector2(x, y);
+        public float Rect_get_width(object rect) => ((Rect)rect).width;
+        public float Rect_get_height(object rect) => ((Rect)rect).height;
+        public bool Input_GetKeyDown(int key) => Input.GetKeyDown((KeyCode)key);
         public object GUI_Window(int id, object clientRect, Action<int> func, string title) => GUI.Window(id, (Rect)clientRect, func, new GUIContent(title), GUI.skin.window);
         public bool GUI_Button(object position, string text) => GUI.Button((Rect)position, text);
-        public bool Input_GetKeyDown(int key) => Input.GetKeyDown((KeyCode)key);
+        public object GUI_BeginScrollView(object position, object scrollPosition, object viewRect) => GUI.BeginScrollView((Rect)position, (Vector2)scrollPosition, (Rect)viewRect, false, false, GUI.skin.horizontalScrollbar, GUI.skin.verticalScrollbar, GUI.skin.window);
+        public void GUI_EndScrollView(bool handleScrollWheel = false) => GUI.EndScrollView(handleScrollWheel);
+
     }
 
-    class RenderHelperGUIFix
+    class RenderHelper_GUIFix
     {
         internal static GUIContent[] GUIContent_Temp(string[] texts)
         {
