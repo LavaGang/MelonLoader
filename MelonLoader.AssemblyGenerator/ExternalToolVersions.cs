51c954048d38f059af0bb26f07a1ec4c4ec4d0f5
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-06-13 17:58:46 +0200

    Update unhollower to 0.4.6.0

--- MelonLoader.AssemblyGenerator/ExternalToolVersions.cs
+++ MelonLoader.AssemblyGenerator/ExternalToolVersions.cs
@@ -5,8 +5,8 @@ namespace MelonLoader.AssemblyGenerator
         public static string Il2CppDumperUrl = "https://github.com/Perfare/Il2CppDumper/releases/download/v6.2.1/Il2CppDumper-v6.2.1.zip";
         public static string Il2CppDumperVersion = "6.2.1";
         
-        public static string Il2CppAssemblyUnhollowerUrl = "https://github.com/knah/Il2CppAssemblyUnhollower/releases/download/v0.4.5.0/Il2CppAssemblyUnhollower.0.4.5.0.zip";
-        public static string Il2CppAssemblyUnhollowerVersion = "0.4.5.0";
+        public static string Il2CppAssemblyUnhollowerUrl = "https://github.com/knah/Il2CppAssemblyUnhollower/releases/download/v0.4.6.0/Il2CppAssemblyUnhollower.0.4.6.0.zip";
+        public static string Il2CppAssemblyUnhollowerVersion = "0.4.6.0";
         
         public static string UnityDependenciesBaseUrl = "https://github.com/HerpDerpinstine/MelonLoader/raw/master/BaseLibs/UnityDependencies/";
     }
