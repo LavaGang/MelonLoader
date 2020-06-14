3f7b6221c327cb3fd5ba60b48a44f282b605088d
zeobviouslyfakeacc <6228668-zeobviouslyfakeacc@users.noreply.gitlab.com>
2020-06-14 03:46:52 +0200

    Improve IL2CPP -> Harmony patch shim - Add support for strings - Add support for most ref types - Fix bug if null IL2CPP argument values were used - Don't crash if an exception gets thrown in managed code - Use knah's new method to find the correct IL2CPP target method

--- MelonLoader.ModHandler/UnhollowerSupport.cs
+++ MelonLoader.ModHandler/UnhollowerSupport.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 
 namespace MelonLoader
@@ -10,22 +8,30 @@ namespace MelonLoader
         private static Assembly UnhollowerBaseLib = null;
         private static Type Il2CppObjectBaseType = null;
         internal static MethodInfo Il2CppObjectBaseToPtrMethod = null;
+        internal static MethodInfo Il2CppStringToManagedMethod = null;
+        internal static MethodInfo ManagedStringToIl2CppMethod = null;
+        internal static MethodInfo GetIl2CppMethodInfoPointerFieldForGeneratedMethod = null;
 
         internal static void Initialize()
         {
             UnhollowerBaseLib = Assembly.Load("UnhollowerBaseLib");
             Il2CppObjectBaseType = UnhollowerBaseLib.GetType("UnhollowerBaseLib.Il2CppObjectBase");
             Il2CppObjectBaseToPtrMethod = UnhollowerBaseLib.GetType("UnhollowerBaseLib.IL2CPP").GetMethod("Il2CppObjectBaseToPtr");
+            Il2CppStringToManagedMethod = UnhollowerBaseLib.GetType("UnhollowerBaseLib.IL2CPP").GetMethod("Il2CppStringToManaged");
+            ManagedStringToIl2CppMethod = UnhollowerBaseLib.GetType("UnhollowerBaseLib.IL2CPP").GetMethod("ManagedStringToIl2Cpp");
+            GetIl2CppMethodInfoPointerFieldForGeneratedMethod = UnhollowerBaseLib.GetType("UnhollowerBaseLib.UnhollowerUtils").GetMethod("GetIl2CppMethodInfoPointerFieldForGeneratedMethod");
         }
 
         internal static bool IsGeneratedAssemblyType(Type type) => (Imports.IsIl2CppGame() && !Il2CppObjectBaseType.Equals(null) && !type.Equals(null) && type.IsSubclassOf(Il2CppObjectBaseType));
 
         internal static IntPtr MethodBaseToIntPtr(MethodBase method)
         {
-            FieldInfo methodptr = method.DeclaringType.GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(x => x.Name.StartsWith(("NativeMethodInfoPtr_" + method.Name)));
-            if (methodptr != null)
+            if (IsGeneratedAssemblyType(method.DeclaringType))
+            {
+                FieldInfo methodptr = (FieldInfo) GetIl2CppMethodInfoPointerFieldForGeneratedMethod.Invoke(null, new object[] { method });
                 return (IntPtr)methodptr.GetValue(null);
-            return IntPtr.Zero;
+            }
+            return method.MethodHandle.GetFunctionPointer();
         }
     }
 }
