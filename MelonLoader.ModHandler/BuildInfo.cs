dfd0b77c8cca05f10ed26602806a1af243e5fdca
Herp Derpinstine <herpderpinstine@gmail.com>
2020-06-16 01:50:32 -0500

    Cleanup

--- MelonLoader.ModHandler/BuildInfo.cs
+++ MelonLoader.ModHandler/BuildInfo.cs
@@ -5,6 +5,6 @@
         public const string Name = "MelonLoader";
         public const string Author = "Lava Gang";
         public const string Company = "discord.gg/2Wn3N2P";
-        public const string Version = "0.2.3";
+        public const string Version = "0.2.2";
     }
 }
