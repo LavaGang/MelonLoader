ffada624ea486bb92803f2cdc6ddcc58750dbdf7
Herp Derpinstine <herpderpinstine@gmail.com>
2020-06-18 17:13:13 -0500

    Temporarily Fixed Tls Issue with Certain Il2Cpp Unity Games like Audica for example

--- MelonLoader.Support.Il2Cpp/Main.cs
+++ MelonLoader.Support.Il2Cpp/Main.cs
@@ -34,13 +34,26 @@ namespace MelonLoader.Support
 
             try
             {
-                unsafe {
-                    var tlsHookTarget = typeof(Uri).Assembly.GetType("Mono.Unity.UnityTls").GetMethod("GetUnityTlsInterface", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).MethodHandle.GetFunctionPointer();
-                    var unityMethodField = UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(typeof(Il2CppMono.Unity.UnityTls).GetMethod("GetUnityTlsInterface", BindingFlags.Public | BindingFlags.Static));
-                    var unityMethodPtr = (IntPtr) unityMethodField.GetValue(null);
-                    var unityMethod = *(IntPtr*) unityMethodPtr;
-                    Imports.Hook((IntPtr)(&tlsHookTarget), unityMethod);
+                Assembly il2cppSystem = Assembly.Load("Il2CppSystem");
+                if (il2cppSystem != null)
+                {
+                    Type unitytls = il2cppSystem.GetType("Il2CppMono.Unity.UnityTls");
+                    if (unitytls != null)
+                    {
+                        unsafe
+                        {
+                            var tlsHookTarget = typeof(Uri).Assembly.GetType("Mono.Unity.UnityTls").GetMethod("GetUnityTlsInterface", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).MethodHandle.GetFunctionPointer();
+                            var unityMethodField = UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(unitytls.GetMethod("GetUnityTlsInterface", BindingFlags.Public | BindingFlags.Static));
+                            var unityMethodPtr = (IntPtr)unityMethodField.GetValue(null);
+                            var unityMethod = *(IntPtr*)unityMethodPtr;
+                            Imports.Hook((IntPtr)(&tlsHookTarget), unityMethod);
+                        }
+                    }
+                    else
+                        throw new Exception("Failed to get Type Il2CppMono.Unity.UnityTls!");
                 }
+                else
+                    throw new Exception("Failed to get Assembly Il2CppSystem!");
             } catch(Exception ex) {
                 MelonModLogger.LogWarning("Exception while setting up TLS, mods will not be able to use HTTPS: " + ex);
             }
