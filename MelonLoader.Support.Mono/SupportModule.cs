26d23e710f414f404842938ca0cf4ab9fe44aad4
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-07-26 15:34:00 +0200

    Create MelonLoaderComponent after first scene load in mono games

--- MelonLoader.Support.Mono/SupportModule.cs
+++ MelonLoader.Support.Mono/SupportModule.cs
@@ -7,8 +7,22 @@ namespace MelonLoader.Support
     public class Module : ISupportModule
     {
         public int GetActiveSceneIndex() => SceneManager.GetActiveScene().buildIndex;
-        public object StartCoroutine(IEnumerator coroutine) => Main.comp.StartCoroutine(coroutine);
-        public void StopCoroutine(object coroutineToken) => Main.comp.StopCoroutine((Coroutine) coroutineToken);
+        public object StartCoroutine(IEnumerator coroutine)
+        {
+            if (Main.comp != null) return Main.comp.StartCoroutine(coroutine);
+
+            MelonLoaderComponent.QueuedCoroutines.Add(coroutine);
+            return coroutine;
+        }
+
+        public void StopCoroutine(object coroutineToken)
+        {
+            if (Main.comp == null)
+                MelonLoaderComponent.QueuedCoroutines.Remove(coroutineToken as IEnumerator);
+            else
+                Main.comp.StopCoroutine(coroutineToken as Coroutine);
+        }
+
         public void UnityDebugLog(string msg) => Debug.Log(msg);
         public void Destroy() => MelonLoaderComponent.Destroy();
     }
