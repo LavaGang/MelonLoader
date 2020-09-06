6bfbf9b15906ecce0aabd01a57e7da8df0ca9a33
Herp Derpinstine <herpderpinstine@gmail.com>
2020-09-05 19:41:34 -0500

    Updated Cpp2Il Version

--- MelonLoader.AssemblyGenerator/ExternalToolVersions.cs
+++ MelonLoader.AssemblyGenerator/ExternalToolVersions.cs
@@ -2,7 +2,7 @@ namespace MelonLoader.AssemblyGenerator
 {
     public static class ExternalToolVersions
     {
-        public static string Cpp2IlVersion = "commit_238ac593f3bc67e4f6bf658c7dc62e002144f144";
+        public static string Cpp2IlVersion = "commit_99f1aedc99a5d543576b24fcbc5da72642fdc258";
         public static string Cpp2IlUrl = "https://github.com/SamboyCoding/Cpp2Il/releases/download/" + Cpp2IlVersion + "/Cpp2Il-Win.exe";
 
         public static string Il2CppAssemblyUnhollowerVersion = "0.4.9.1";
