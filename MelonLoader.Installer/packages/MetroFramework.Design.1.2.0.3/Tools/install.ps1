21124ceeaa80ca2d3a75a2ba72e6437de6441fc5
Herp Derpinstine <herpderpinstine@gmail.com>
2020-12-25 22:08:08 -0600

    Moved Installer to GitHub Repo

--- MelonLoader.Installer/packages/MetroFramework.Design.1.2.0.3/Tools/install.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
- 
-foreach ($reference in $project.Object.References)
-{
-    if($reference.Name -eq "MetroFramework.Design")
-    {
-        $reference.CopyLocal = $false;
-    }
-}
