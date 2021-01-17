059eb738465994f77f30733adb556fabe6e3b173
Akiva Silver <akiva.silver@gmail.com>
2021-01-16 19:28:03 -0500

    created a console app to test rebuilding android apps

---
+++ ManualAndroidPatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManualAndroidPatcher
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Working!");
+        }
+    }
+}
