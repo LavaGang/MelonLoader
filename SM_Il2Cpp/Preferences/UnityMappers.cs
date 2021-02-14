a34f9195c020ad863a985ee45bb72a89ca47ffdd
Herp Derpinstine <herpderpinstine@gmail.com>
2021-02-14 02:33:29 -0600

    Cleanup

--- SM_Il2Cpp/Preferences/UnityMappers.cs
+++ SM_Il2Cpp/Preferences/UnityMappers.cs
@@ -5,7 +5,7 @@ namespace MelonLoader.Support.Preferences
 {
     public static class UnityMappers
     {
-        public static void RegisterMappers()
+        internal static void RegisterMappers()
         {
             MelonPreferences.Mapper.RegisterMapper(ReadColor, WriteColor);
             MelonPreferences.Mapper.RegisterMapper(ReadColor32, WriteColor32);
@@ -15,92 +15,87 @@ namespace MelonLoader.Support.Preferences
             MelonPreferences.Mapper.RegisterMapper(ReadQuaternion, WriteQuaternion);
         }
 
-        public static Color ReadColor(TomlObject value)
+        private static Color ReadColor(TomlObject value)
         {
-            var floats = MelonPreferences.Mapper.ReadArray<float>(value);
+            float[] floats = MelonPreferences.Mapper.ReadArray<float>(value);
             if (floats == null || floats.Length != 4)
                 return default;
-            
             return new Color(floats[0] / 255f, floats[1] / 255f, floats[2] / 255f, floats[3] / 255f);
         }
 
-        public static TomlObject WriteColor(Color value)
+        private static TomlObject WriteColor(Color value)
         {
-            var floats = new[] { value.r * 255, value.g * 255, value.b * 255, value.a * 255};
+            float[] floats = new[] { value.r * 255, value.g * 255, value.b * 255, value.a * 255};
             return MelonPreferences.Mapper.WriteArray(floats);
         }
 
-        public static Color32 ReadColor32(TomlObject value)
+        private static Color32 ReadColor32(TomlObject value)
         {
-            var bytes = MelonPreferences.Mapper.ReadArray<byte>(value);
+            byte[] bytes = MelonPreferences.Mapper.ReadArray<byte>(value);
             if (bytes == null || bytes.Length != 4)
                 return default;
             return new Color32(bytes[0], bytes[1], bytes[2], bytes[3]);
         }
 
-        public static TomlObject WriteColor32(Color32 value)
+        private static TomlObject WriteColor32(Color32 value)
         {
-            var bytes = new[] { value.r, value.g, value.b, value.a };
+            byte[] bytes = new[] { value.r, value.g, value.b, value.a };
             return MelonPreferences.Mapper.WriteArray(bytes);
         }
 
-        public static Vector2 ReadVector2(TomlObject value)
+        private static Vector2 ReadVector2(TomlObject value)
         {
-            var floats = MelonPreferences.Mapper.ReadArray<float>(value);
+            float[] floats = MelonPreferences.Mapper.ReadArray<float>(value);
             if (floats == null || floats.Length != 2)
                 return default;
-            
             return new Vector2(floats[0], floats[1]);
         }
 
-        public static TomlObject WriteVector2(Vector2 value)
+        private static TomlObject WriteVector2(Vector2 value)
         {
-            var floats = new float[] { value.x, value.y };
+            float[] floats = new[] { value.x, value.y };
             return MelonPreferences.Mapper.WriteArray(floats);
         }
-        
-        public static Vector3 ReadVector3(TomlObject value)
+
+        private static Vector3 ReadVector3(TomlObject value)
         {
-            var floats = MelonPreferences.Mapper.ReadArray<float>(value);
+            float[] floats = MelonPreferences.Mapper.ReadArray<float>(value);
             if (floats == null || floats.Length != 3)
                 return default;
-            
             return new Vector3(floats[0], floats[1], floats[2]);
         }
 
-        public static TomlObject WriteVector3(Vector3 value)
+        private static TomlObject WriteVector3(Vector3 value)
         {
-            var floats = new float[] { value.x, value.y, value.z };
+            float[] floats = new[] { value.x, value.y, value.z };
             return MelonPreferences.Mapper.WriteArray(floats);
         }
-        
-        public static Vector4 ReadVector4(TomlObject value)
+
+        private static Vector4 ReadVector4(TomlObject value)
         {
-            var floats = MelonPreferences.Mapper.ReadArray<float>(value);
+            float[] floats = MelonPreferences.Mapper.ReadArray<float>(value);
             if (floats == null || floats.Length != 4)
                 return default;
-            
             return new Vector4(floats[0], floats[1], floats[2], floats[3]);
         }
 
-        public static TomlObject WriteVector4(Vector4 value)
+        private static TomlObject WriteVector4(Vector4 value)
         {
-            var floats = new float[] { value.x, value.y, value.z, value.w };
+            float[] floats = new[] { value.x, value.y, value.z, value.w };
             return MelonPreferences.Mapper.WriteArray(floats);
         }
-        
-        public static Quaternion ReadQuaternion(TomlObject value)
+
+        private static Quaternion ReadQuaternion(TomlObject value)
         {
-            var floats = MelonPreferences.Mapper.ReadArray<float>(value);
+            float[] floats = MelonPreferences.Mapper.ReadArray<float>(value);
             if (floats == null || floats.Length != 4)
                 return default;
-            
             return new Quaternion(floats[0], floats[1], floats[2], floats[3]);
         }
 
-        public static TomlObject WriteQuaternion(Quaternion value)
+        private static TomlObject WriteQuaternion(Quaternion value)
         {
-            var floats = new float[] { value.x, value.y, value.z, value.w };
+            float[] floats = new[] { value.x, value.y, value.z, value.w };
             return MelonPreferences.Mapper.WriteArray(floats);
         }
     }
