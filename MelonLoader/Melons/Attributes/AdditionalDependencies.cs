6cd03603405d4cbd60204c27d71a506360bbfa03
Herp Derpinstine <herpderpinstine@gmail.com>
2021-02-28 21:48:15 -0600

    Cleanup

--- MelonLoader/Melons/Attributes/AdditionalDependencies.cs
+++ MelonLoader/Melons/Attributes/AdditionalDependencies.cs
@@ -6,7 +6,7 @@ namespace MelonLoader
     public class MelonAdditionalDependenciesAttribute : Attribute
     {
         /// <summary>
-        /// The (simple) assembly names of Additional Dependencies that aren't directly referenced but should still be regarded.
+        /// The (simple) assembly names of Additional Dependencies that aren't directly referenced but should still be regarded as important.
         /// </summary>
         public string[] AssemblyNames { get; internal set; }
 
