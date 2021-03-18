276ff8bd4de20410fa92d18327ae46dc435bda3b
Herp Derpinstine <herpderpinstine@gmail.com>
2021-03-18 07:26:29 -0500

    Cleanup

---
+++ MelonLoader/CompatibilityLayers/IPA/IEnhancedPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IllusionPlugin
+{
+	[Obsolete("IllusionPlugin.IEnhancedPlugin is Only Here for Compatibility Reasons. Please use MelonPlugin or MelonMod instead.")]
+	public interface IEnhancedPlugin : IPlugin
+	{
+		[Obsolete("IllusionPlugin.IEnhancedPlugin.Filter is Only Here for Compatibility Reasons. Please use the MelonGame Attribute instead.")]
+		string[] Filter { get; }
+		[Obsolete("IllusionPlugin.IEnhancedPlugin.OnLateUpdate() is Only Here for Compatibility Reasons. Please use MelonPlugin.OnLateUpdate() or MelonMod.OnLateUpdate() instead.")]
+		void OnLateUpdate();
+	}
+}
---
+++ MelonLoader/CompatibilityLayers/IPA/IPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IllusionPlugin
+{
+	[Obsolete("IllusionPlugin.IPlugin is Only Here for Compatibility Reasons. Please use MelonPlugin or MelonMod instead.")]
+	public interface IPlugin
+	{
+		[Obsolete("IllusionPlugin.IPlugin.Name is Only Here for Compatibility Reasons. Please use MelonPlugin.Info.Name or MelonMod.Info.Name instead.")]
+		string Name { get; }
+		[Obsolete("IllusionPlugin.IPlugin.Version is Only Here for Compatibility Reasons. Please use MelonPlugin.Info.Version or MelonMod.Info.Version instead.")]
+		string Version { get; }
+		[Obsolete("IllusionPlugin.IPlugin.OnApplicationStart() is Only Here for Compatibility Reasons. Please use MelonPlugin.OnApplicationStart() or MelonMod.OnApplicationStart() instead.")]
+		void OnApplicationStart();
+		[Obsolete("IllusionPlugin.IPlugin.OnApplicationQuit() is Only Here for Compatibility Reasons. Please use MelonPlugin.OnApplicationQuit() or MelonMod.OnApplicationQuit() instead.")]
+		void OnApplicationQuit();
+		[Obsolete("IllusionPlugin.IPlugin.OnLevelWasLoaded(int) is Only Here for Compatibility Reasons. Please use MelonPlugin.OnSceneWasLoaded(int, string) or MelonMod.OnSceneWasLoaded(int, string) instead.")]
+		void OnLevelWasLoaded(int level);
+		[Obsolete("IllusionPlugin.IPlugin.OnLevelWasInitialized(int) is Only Here for Compatibility Reasons. Please use MelonPlugin.OnSceneWasInitialized(int, string) or MelonMod.OnSceneWasInitialized(int, string) instead.")]
+		void OnLevelWasInitialized(int level);
+		[Obsolete("IllusionPlugin.IPlugin.OnUpdate() is Only Here for Compatibility Reasons. Please use MelonPlugin.OnUpdate() or MelonMod.OnUpdate() instead.")]
+		void OnUpdate();
+		[Obsolete("IllusionPlugin.IPlugin.OnFixedUpdate() is Only Here for Compatibility Reasons. Please use MelonMod.OnFixedUpdate() instead.")]
+		void OnFixedUpdate();
+	}
+}
