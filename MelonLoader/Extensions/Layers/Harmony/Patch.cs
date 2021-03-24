d5bf4333fb19179835b10a1907d005ad390141c4
Herp Derpinstine <herpderpinstine@gmail.com>
2021-03-23 21:00:30 -0500

    Cleanup

--- MelonLoader/Extensions/Layers/Harmony/Patch.cs
+++ MelonLoader/Extensions/Layers/Harmony/Patch.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using HarmonyLib;
 
 namespace Harmony
 {
--- MelonLoader/Extensions/Layers/Harmony/Tools/SymbolExtensions.cs
+++ MelonLoader/Extensions/Layers/Harmony/Tools/SymbolExtensions.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Linq.Expressions;
 using System.Reflection;
-using System;
 
 namespace Harmony
 {
