43507cf011a7e90d15ae0103e64d22d61750ebc8
Herp Derpinstine <herpderpinstine@gmail.com>
2021-03-23 21:21:41 -0500

    Cleanup

--- MelonLoader/Extensions/Layers/Harmony/HarmonyInstance.cs
+++ MelonLoader/Extensions/Layers/Harmony/HarmonyInstance.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-#pragma warning disable 0108
 #pragma warning disable 0618
 
 namespace Harmony
@@ -20,9 +17,6 @@ namespace Harmony
 			return new HarmonyInstance(id);
 		}
 
-		public void PatchAll() => base.PatchAll();
-		public void PatchAll(Assembly assembly) => base.PatchAll(assembly);
-
 		[Obsolete("Harmony.HarmonyInstance.Patch is obsolete. Please use HarmonyLib.Harmony.Patch instead.")]
 		public DynamicMethod Patch(MethodBase original, HarmonyMethod prefix = null, HarmonyMethod postfix = null, HarmonyMethod transpiler = null)
 		{
@@ -30,16 +24,6 @@ namespace Harmony
 			return null;
 		}
 
-		public void UnpatchAll(string harmonyID = null) => base.UnpatchAll(harmonyID);
         public void Unpatch(MethodBase original, HarmonyPatchType type, string harmonyID = null) => Unpatch(original, (HarmonyLib.HarmonyPatchType)type, harmonyID);
-        public void Unpatch(MethodBase original, MethodInfo patch) => base.Unpatch(original, patch);
-
-		public bool HasAnyPatches(string harmonyID) =>
-			GetPatchedMethods()
-				.Select(original => GetPatchInfo(original))
-				.Any(info => info.Owners.Contains(harmonyID));
-
-		public IEnumerable<MethodBase> GetPatchedMethods() => base.GetPatchedMethods();
-		public Dictionary<string, Version> VersionInfo(out Version currentVersion) { currentVersion = null; return null; }
 	}
 }
