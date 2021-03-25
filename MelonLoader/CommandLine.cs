deff2f0645df892f7ed69fa85a563dee2eb9d87b
Herp Derpinstine <herpderpinstine@gmail.com>
2021-03-24 20:23:57 -0500

    Cleanup

--- MelonLoader/CommandLine.cs
+++ MelonLoader/CommandLine.cs
@@ -6,12 +6,6 @@ namespace MelonLoader
 {
     public static class MelonCommandLine
     {
-        public static Args.AnalyticsBlocker AnalyticsBlocker = new Args.AnalyticsBlocker();
-        public static Args.AssemblyGenerator AssemblyGenerator = new Args.AssemblyGenerator();
-        public static Args.Console Console = new Args.Console();
-        public static Args.Core Core = new Args.Core();
-        public static Args.Logger Logger = new Args.Logger();
-
         private class ArgInfo
         {
             internal string CommandLine;
@@ -138,25 +132,25 @@ namespace MelonLoader
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.magenta",
-                ParseCommandLineArg = (string value) => Console.DisplayMode = Args.Console.Enum.MAGENTA
+                ParseCommandLineArg = (string value) => Console.DisplayMode = Console.DisplayModeEnum.MAGENTA
             });
 
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.rainbow",
-                ParseCommandLineArg = (string value) => Console.DisplayMode = Args.Console.Enum.RAINBOW
+                ParseCommandLineArg = (string value) => Console.DisplayMode = Console.DisplayModeEnum.RAINBOW
             });
 
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.randomrainbow",
-                ParseCommandLineArg = (string value) => Console.DisplayMode = Args.Console.Enum.RANDOMRAINBOW
+                ParseCommandLineArg = (string value) => Console.DisplayMode = Console.DisplayModeEnum.RANDOMRAINBOW
             });
 
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--lemonloader",
-                ParseCommandLineArg = (string value) => Console.DisplayMode = Args.Console.Enum.LEMON
+                ParseCommandLineArg = (string value) => Console.DisplayMode = Console.DisplayModeEnum.LEMON
             });
 
             ArgInfoTbl.Add(new ArgInfo
@@ -166,9 +160,9 @@ namespace MelonLoader
                 ParseConfigArg = (ArgInfo argInfo) =>
                 {
                     int mode = iniFile.GetInt(argInfo.Config_Category, argInfo.Config_Name, (int)Console.DisplayMode, true);
-                    if ((mode >= (int)Args.Console.Enum.NORMAL)
-                        && (mode <= (int)Args.Console.Enum.LEMON))
-                        Console.DisplayMode = (Args.Console.Enum)mode;
+                    if ((mode >= (int)Console.DisplayModeEnum.NORMAL)
+                        && (mode <= (int)Console.DisplayModeEnum.LEMON))
+                        Console.DisplayMode = (Console.DisplayModeEnum)mode;
                 }
             });
             #endregion
@@ -181,9 +175,9 @@ namespace MelonLoader
                 {
                     int mode = 0;
                     if (int.TryParse(value, out mode)
-                        && (mode >= (int)Args.Core.Enum.NORMAL)
-                        && (mode <= (int)Args.Core.Enum.BOTH))
-                        Core.LoadMode_Plugins = (Args.Core.Enum)mode;
+                        && (mode >= (int)Core.LoadModeEnum.NORMAL)
+                        && (mode <= (int)Core.LoadModeEnum.BOTH))
+                        Core.LoadMode_Plugins = (Core.LoadModeEnum)mode;
                 },
 
                 Config_Name = "LoadMode_Plugins",
@@ -191,9 +185,9 @@ namespace MelonLoader
                 ParseConfigArg = (ArgInfo argInfo) =>
                 {
                     int mode = iniFile.GetInt(argInfo.Config_Category, argInfo.Config_Name, (int)Core.LoadMode_Plugins, true);
-                    if ((mode >= (int)Args.Core.Enum.NORMAL)
-                        && (mode <= (int)Args.Core.Enum.BOTH))
-                        Core.LoadMode_Plugins = (Args.Core.Enum)mode;
+                    if ((mode >= (int)Core.LoadModeEnum.NORMAL)
+                        && (mode <= (int)Core.LoadModeEnum.BOTH))
+                        Core.LoadMode_Plugins = (Core.LoadModeEnum)mode;
                 }
             });
 
@@ -204,9 +198,9 @@ namespace MelonLoader
                 {
                     int mode = 0;
                     if (int.TryParse(value, out mode)
-                        && (mode >= (int)Args.Core.Enum.NORMAL)
-                        && (mode <= (int)Args.Core.Enum.BOTH))
-                        Core.LoadMode_Mods = (Args.Core.Enum)mode;
+                        && (mode >= (int)Core.LoadModeEnum.NORMAL)
+                        && (mode <= (int)Core.LoadModeEnum.BOTH))
+                        Core.LoadMode_Mods = (Core.LoadModeEnum)mode;
                 },
 
                 Config_Name = "LoadMode_Mods",
@@ -214,9 +208,9 @@ namespace MelonLoader
                 ParseConfigArg = (ArgInfo argInfo) =>
                 {
                     int mode = iniFile.GetInt(argInfo.Config_Category, argInfo.Config_Name, (int)Core.LoadMode_Mods, true);
-                    if ((mode >= (int)Args.Core.Enum.NORMAL)
-                        && (mode <= (int)Args.Core.Enum.BOTH))
-                        Core.LoadMode_Mods = (Args.Core.Enum)mode;
+                    if ((mode >= (int)Core.LoadModeEnum.NORMAL)
+                        && (mode <= (int)Core.LoadModeEnum.BOTH))
+                        Core.LoadMode_Mods = (Core.LoadModeEnum)mode;
                 }
             });
 
@@ -247,7 +241,7 @@ namespace MelonLoader
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.maxlogs",
-                ParseCommandLineArg = (string value) => int.TryParse(value, out Logger.Max_Logs),
+                ParseCommandLineArg = (string value) => { int val = Logger.Max_Logs; if (int.TryParse(value, out val)) Logger.Max_Logs = val; },
 
                 Config_Name = "Max_Logs",
                 Config_Category = Config_Category_Logger,
@@ -257,7 +251,7 @@ namespace MelonLoader
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.maxwarnings",
-                ParseCommandLineArg = (string value) => int.TryParse(value, out Logger.Max_Warnings),
+                ParseCommandLineArg = (string value) => { int val = Logger.Max_Warnings; if (int.TryParse(value, out val)) Logger.Max_Warnings = val; },
 
                 Config_Name = "Max_Warnings",
                 Config_Category = Config_Category_Logger,
@@ -267,7 +261,7 @@ namespace MelonLoader
             ArgInfoTbl.Add(new ArgInfo
             {
                 CommandLine = "--melonloader.maxerrors",
-                ParseCommandLineArg = (string value) => int.TryParse(value, out Logger.Max_Errors),
+                ParseCommandLineArg = (string value) => { int val = Logger.Max_Errors; if (int.TryParse(value, out val)) Logger.Max_Errors = val; },
                 
                 Config_Name = "Max_Errors",
                 Config_Category = Config_Category_Logger,
@@ -331,64 +325,90 @@ namespace MelonLoader
             return null;
         }
 
-        public class Args
+        #region Args
+        public static class AnalyticsBlocker
         {
-            public class AnalyticsBlocker
-            {
-                public bool ShouldDAB = false;
-            }
-
-            public class AssemblyGenerator
-            {
-                public bool ForceRegeneration = false;
-                public string ForceVersion_Dumper = "0.0.0.0";
-                public string ForceVersion_Il2CppAssemblyUnhollower = "0.0.0.0";
-                public string ForceVersion_UnityDependencies = "0.0.0.0";
-            }
-
-            public class Console
-            {
-                public enum Enum
-                {
-                    NORMAL,
-                    MAGENTA,
-                    RAINBOW,
-                    RANDOMRAINBOW,
-                    LEMON
-                }
-                public Enum DisplayMode = Enum.NORMAL;
-
-                public bool Enabled = true;
-                public bool AlwaysOnTop = false;
-                public bool HideWarnings = false;
-                public bool SetTitleOnInit = true;
-            }
-
-            public class Core
-            {
-                public enum Enum
-                {
-                    NORMAL,
-                    DEV,
-                    BOTH
-                }
-                public Enum LoadMode_Plugins = Enum.NORMAL;
-                public Enum LoadMode_Mods = Enum.NORMAL;
-
-#if DEBUG
-                public bool DebugMode = true;
-#else
-                public bool DebugMode = false;
-#endif
-                public bool QuitFix = false;
-            }
-
-            public class Logger
-            {
-                public int Max_Logs = 10;
-                public int Max_Warnings = 100;
-                public int Max_Errors = 100;
-            }
+            static AnalyticsBlocker() => ShouldDAB = false;
+            public static bool ShouldDAB { get; internal set; }
         }
+
+        public static class AssemblyGenerator
+        {
+            static AssemblyGenerator()
+            {
+                ForceRegeneration = false;
+                ForceVersion_Dumper = "0.0.0.0";
+                ForceVersion_Il2CppAssemblyUnhollower = "0.0.0.0";
+                ForceVersion_UnityDependencies = "0.0.0.0";
+            }
+            public static bool ForceRegeneration { get; internal set; }
+            public static string ForceVersion_Dumper { get; internal set; }
+            public static string ForceVersion_Il2CppAssemblyUnhollower { get; internal set; }
+            public static string ForceVersion_UnityDependencies { get; internal set; }
+        }
+
+        public static class Console
+        {
+            static Console()
+            {
+                DisplayMode = DisplayModeEnum.NORMAL;
+                Enabled = true;
+                AlwaysOnTop = false;
+                HideWarnings = false;
+                SetTitleOnInit = true;
+            }
+            public enum DisplayModeEnum
+            {
+                NORMAL,
+                MAGENTA,
+                RAINBOW,
+                RANDOMRAINBOW,
+                LEMON
+            }
+            public static DisplayModeEnum DisplayMode { get; internal set; }
+            public static bool Enabled { get; internal set; }
+            public static bool AlwaysOnTop { get; internal set; }
+            public static bool HideWarnings { get; internal set; }
+            public static bool SetTitleOnInit { get; internal set; }
+        }
+
+        public static class Core
+        {
+            static Core()
+            {
+                LoadMode_Plugins = LoadModeEnum.NORMAL;
+                LoadMode_Mods = LoadModeEnum.NORMAL;
+                QuitFix = false;
+#if DEBUG
+                DebugMode = true;
+#else
+                DebugMode = false;
+#endif
+            }
+            public enum LoadModeEnum
+            {
+                NORMAL,
+                DEV,
+                BOTH
+            }
+            public static LoadModeEnum LoadMode_Plugins { get; internal set; }
+            public static LoadModeEnum LoadMode_Mods { get; internal set; }
+            public static bool DebugMode { get; internal set; }
+            public static bool QuitFix { get; internal set; }
+        }
+
+        public static class Logger
+        {
+            static Logger()
+            {
+                Max_Logs = 10;
+                Max_Warnings = 100;
+                Max_Errors = 100;
+            }
+            public static int Max_Logs { get; internal set; }
+            public static int Max_Warnings { get; internal set; }
+            public static int Max_Errors { get; internal set; }
+        }
+        #endregion
     }
 }
