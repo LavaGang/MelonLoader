f0489e2ec9539857be86ac5a7116b33178b46886
Akiva Silver <akiva.silver@pm.me>
2021-04-21 04:11:45 -0400

    started making bindings for Java

---
+++ Bootstrap/External/bHapticsBridge.cpp
@@ -0,0 +1,9 @@
+#include "bHapticsBridge.h"
+#include <jni.h>
+
+extern "C" {
+	void onDeviceUpdate_native(JNIEnv* env, jclass type, jobjectArray jDevices)
+	{
+		jsize deviceCount = env->GetArrayLength(jDevices);
+	}
+}
---
+++ Bootstrap/External/bHapticsBridge.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+class bHapticsBridge
+{
+public:
+	class Core 
+	{
+	public:
+		struct t_options
+		{
+			float intensity;
+			float duration;
+			float offsetAngleX;
+			float offsetY;
+		};
+
+		static void start();
+		static void stop();
+		static void turnOff(const char*);
+		static void turnOffAll();
+		static void registerProject(const char*, const char*);
+		static void registerProjectReflected(const char*, const char*);
+		static void submitRegisteredWithOption(const char*, const char*, t_options);
+		static void submitRegisteredWithTime(const char*, int);
+		static void submitDotArray(const char*, const char*, int*, size_t, int*, size_t, int);
+		static void submitPathArray(const char*, const char*, int*, size_t, int*, size_t, int);
+		static std::vector<int> getPositionStatus(const char*);
+		static bool isRegistered(const char*);
+		static bool isPlaying(const char*);
+		static bool isAnythingPlaying(const char*);
+	};
+};
+
