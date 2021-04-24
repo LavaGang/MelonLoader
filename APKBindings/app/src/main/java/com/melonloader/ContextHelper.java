b4b6c627bb67bd47a4de53ef7bc5328c6e619d57
Akiva Silver <akiva.silver@pm.me>
2021-04-23 20:54:40 -0400

    Added a nice context checker wrapper

--- APKBindings/app/src/main/java/com/melonloader/ContextHelper.java
+++ APKBindings/app/src/main/java/com/melonloader/ContextHelper.java
@@ -1,6 +1,11 @@
 package com.melonloader;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
+
+import com.melonloader.bridge.Assertion;
+import com.melonloader.exceptions.MissingContext;
 
 public class ContextHelper {
     public static void DefineContext(Context context)
@@ -9,5 +14,17 @@ public class ContextHelper {
         ApplicationState.Context = context;
 
         LogBridge.msg("Application Context Defined");
+
+        AssemblyHelper.InstallAssemblies();
+    }
+
+    public static boolean CheckContext(String error) {
+        if (!ApplicationState.ContextDefined) {
+            new MissingContext(error).printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
+
