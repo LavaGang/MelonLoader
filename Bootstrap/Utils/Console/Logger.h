ec0026872c6c2618ed534287daddf3bbb2c92f97
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-05-08 19:54:41 -0400

    Properly free char*

--- Bootstrap/Utils/Console/Logger.h
+++ Bootstrap/Utils/Console/Logger.h
@@ -95,7 +95,12 @@ private:
 
 		// Thanks sc2ad for this pretty good code
 		// Logic is now here so it's lifetime is guaranteed to be as long as we need it to be
-		LogArgs(Console::Color color_, LogLevel level_, const MessagePrefix prefixes_[], const int size, const char* fmt, va_list args) : txtcolor(color_), level(level_),prefixes_len(size), prefixes(make_prefixes(prefixes_, size)), buffer(make_buffer(fmt, args)) {}
+		LogArgs(Console::Color color_, LogLevel level_, const MessagePrefix prefixes_[], const int size, const char* fmt, va_list args) : txtcolor(color_), level(level_),prefixes_len(size), prefixes(make_prefixes(prefixes_, size))
+		{
+			auto charBuffer = make_buffer(fmt, args);
+			buffer.assign(charBuffer);
+			delete charBuffer;
+		}
 	private:
 		 static std::vector<MessagePrefix> make_prefixes(const MessagePrefix prefixes_[], const int size) {
 			auto prefixes = std::vector<MessagePrefix>(size);
