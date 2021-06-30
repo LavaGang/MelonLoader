54d44dd9d512e4eff91d3350b7dfba94447e4060
Herp Derpinstine <herpderpinstine@gmail.com>
2021-06-30 13:17:19 -0500

    Cleanup

--- SMCL_Demeo/Main.cs
+++ SMCL_Demeo/Main.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using MelonLoader;
-using HarmonyLib;
 using Boardgame.Modding;
 #pragma warning disable 0618
 
