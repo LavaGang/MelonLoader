4e4fe48b62cb6a7011a70b5081f030a3002b4daf
Herp Derpinstine <herpderpinstine@gmail.com>
2021-09-26 22:21:18 -0500

    Added Unity Game Check to Proxy #103

--- Proxy/Core.h
+++ Proxy/Core.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>
 #include <string>
+#include <filesystem>
 
 class Core
 {
@@ -8,11 +9,10 @@ public:
 	static void Load(HINSTANCE hinstDLL);
 
 private:
+	static bool IsUnityCrashHandler(std::string exe_filename) { return (strstr(exe_filename.c_str(), "unitycrashhandler") != NULL); };
+	static HMODULE LoadOriginalDLL(std::string proxy_filename, std::string proxy_filename_no_ext);
 	static std::string GetBootstrapPath();
-	static void ApplicationCheck();
+	static bool IsUnityGame(std::string exe_filename);
+	static void Error(std::string reason, bool should_kill = false);
 	static void KillItDead();
-	static void LoadExports_psapi(HMODULE originaldll);
-	static void LoadExports_version(HMODULE originaldll);
-	static void LoadExports_winmm(HMODULE originaldll);
-	static void LoadExports_winhttp(HMODULE originaldll);
 };
--- Proxy/Exports/Ex_psapi.cpp
+++ Proxy/Exports/Ex_psapi.cpp
@@ -1,39 +1,40 @@
-#include "../Core.h"
+#include "../Exports.h"
 
 extern "C" FARPROC OriginalFuncs_psapi[27];
 FARPROC OriginalFuncs_psapi[27];
-const char* ExportNames_psapi[] = {
-	"EmptyWorkingSet",
-	"EnumDeviceDrivers",
-	"EnumPageFilesA",
-	"EnumPageFilesW",
-	"EnumProcessModules",
-	"EnumProcessModulesEx",
-	"EnumProcesses",
-	"GetDeviceDriverBaseNameA",
-	"GetDeviceDriverBaseNameW",
-	"GetDeviceDriverFileNameA",
-	"GetDeviceDriverFileNameW",
-	"GetMappedFileNameA",
-	"GetMappedFileNameW",
-	"GetModuleBaseNameA",
-	"GetModuleBaseNameW",
-	"GetModuleFileNameExA",
-	"GetModuleFileNameExW",
-	"GetModuleInformation",
-	"GetPerformanceInfo",
-	"GetProcessImageFileNameA",
-	"GetProcessImageFileNameW",
-	"GetProcessMemoryInfo",
-	"GetWsChanges",
-	"GetWsChangesEx",
-	"InitializeProcessForWsWatch",
-	"QueryWorkingSet",
-	"QueryWorkingSetEx"
-};
 
-void Core::LoadExports_psapi(HMODULE originaldll)
+void Exports::Load_psapi(HMODULE originaldll)
 {
-	for (int i = 0; i < (sizeof(ExportNames_psapi) / sizeof(ExportNames_psapi[0])); i++)
-		OriginalFuncs_psapi[i] = GetProcAddress(originaldll, ExportNames_psapi[i]);
+	const char* ExportNames[] = {
+	   "EmptyWorkingSet",
+	   "EnumDeviceDrivers",
+	   "EnumPageFilesA",
+	   "EnumPageFilesW",
+	   "EnumProcessModules",
+	   "EnumProcessModulesEx",
+	   "EnumProcesses",
+	   "GetDeviceDriverBaseNameA",
+	   "GetDeviceDriverBaseNameW",
+	   "GetDeviceDriverFileNameA",
+	   "GetDeviceDriverFileNameW",
+	   "GetMappedFileNameA",
+	   "GetMappedFileNameW",
+	   "GetModuleBaseNameA",
+	   "GetModuleBaseNameW",
+	   "GetModuleFileNameExA",
+	   "GetModuleFileNameExW",
+	   "GetModuleInformation",
+	   "GetPerformanceInfo",
+	   "GetProcessImageFileNameA",
+	   "GetProcessImageFileNameW",
+	   "GetProcessMemoryInfo",
+	   "GetWsChanges",
+	   "GetWsChangesEx",
+	   "InitializeProcessForWsWatch",
+	   "QueryWorkingSet",
+	   "QueryWorkingSetEx"
+	};
+
+	for (int i = 0; i < (sizeof(ExportNames) / sizeof(ExportNames[0])); i++)
+		OriginalFuncs_psapi[i] = GetProcAddress(originaldll, ExportNames[i]);
 }
