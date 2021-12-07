99885bfa15e4af932dd886bf800527faee048bf0
Herp Derpinstine <herpderpinstine@gmail.com>
2021-12-06 23:17:22 -0600

    Cleanup

--- SM_Il2Cpp/BONEWORKS_SceneHandler.cs
+++ SM_Il2Cpp/BONEWORKS_SceneHandler.cs
@@ -6,13 +6,11 @@
         private static string LastSceneName = null;
         private static bool IsLoading = false;
         private static bool HasFinishedLoading = false;
-        private static bool ShouldWait = false;
         private static bool HasGotLoadingSceneIndex = false;
         private static int LoadingSceneIndex = -9;
 
         internal static void OnSceneLoad(int current_scene, string current_scene_name)
         {
-            bool should_run = true;
             if (!HasGotLoadingSceneIndex)
             {
                 if (current_scene != 0)
@@ -22,36 +20,22 @@
                     LastSceneIndex = current_scene;
                     LastSceneName = current_scene_name;
                     IsLoading = true;
-                    should_run = false;
-                    ShouldWait = false;
                 }
                 else
                 {
                     LastSceneIndex = current_scene;
                     LastSceneName = current_scene_name;
                     IsLoading = true;
-                    should_run = false;
                 }
             }
             else
             {
-                if (current_scene == -1)
-                    should_run = false;
-                else
+                if (current_scene >= 0)
                 {
                     LastSceneIndex = current_scene;
                     LastSceneName = current_scene_name;
                     IsLoading = true;
-                    should_run = false;
-                    ShouldWait = false;
                 }
-            }
-            if (should_run)
-            {
-                LastSceneIndex = current_scene;
-                LastSceneName = current_scene_name;
-                if (!ShouldWait)
-                    IsLoading = true;
             }
         }
 
