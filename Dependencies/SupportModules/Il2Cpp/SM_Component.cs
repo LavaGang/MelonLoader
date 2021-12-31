f3d1c02f5f2e66d9ef14b40460c1699736db5b98
Herp Derpinstine <herpderpinstine@gmail.com>
2021-12-31 00:21:38 -0600

    SM_Component Class Cleanup

--- Dependencies/SupportModules/Il2Cpp/SM_Component.cs
+++ Dependencies/SupportModules/Il2Cpp/SM_Component.cs
@@ -9,8 +9,10 @@ namespace MelonLoader.Support
         private static bool IsDestroying = false;
         private delegate bool SetAsLastSiblingDelegate(IntPtr transformptr);
         private static SetAsLastSiblingDelegate SetAsLastSiblingDelegateField;
+
         public SM_Component(IntPtr value) : base(value) { }
         static SM_Component() => SetAsLastSiblingDelegateField = IL2CPP.ResolveICall<SetAsLastSiblingDelegate>("UnityEngine.Transform::SetAsLastSibling");
+
         internal static void Create()
         {
             Main.obj = new GameObject();
@@ -19,36 +21,41 @@ namespace MelonLoader.Support
             Main.component = Main.obj.AddComponent(UnhollowerRuntimeLib.Il2CppType.Of<SM_Component>()).TryCast<SM_Component>();
             Main.component.SiblingFix();
         }
+
         private void SiblingFix()
         {
             SetAsLastSiblingDelegateField(IL2CPP.Il2CppObjectBaseToPtrNotNull(gameObject.transform));
             SetAsLastSiblingDelegateField(IL2CPP.Il2CppObjectBaseToPtrNotNull(transform));
         }
+
         internal void Destroy()
         {
             IsDestroying = true;
             GameObject.Destroy(gameObject);
         }
-        void Start() { SiblingFix(); Main.Interface.OnApplicationLateStart(); }
+
+        void Start()
+        {
+            SiblingFix();
+            Main.Interface.OnApplicationLateStart();
+        }
+
         void Awake()
         {
             foreach (var queuedCoroutine in SupportModule_To.QueuedCoroutines)
                 StartCoroutine(new Il2CppSystem.Collections.IEnumerator(new MonoEnumeratorWrapper(queuedCoroutine).Pointer));
             SupportModule_To.QueuedCoroutines.Clear();
         }
+
         void Update()
         {
             SiblingFix();
             if (MelonUtils.IsBONEWORKS)
                 BONEWORKS_SceneHandler.OnUpdate();
             Main.Interface.Update();
-            //Coroutines.Process();
         }
-        void FixedUpdate()
-        {
-            Main.Interface.FixedUpdate();
-            //Coroutines.ProcessWaitForFixedUpdate();
-        }
+
+        void FixedUpdate() => Main.Interface.FixedUpdate();
         void LateUpdate() => Main.Interface.LateUpdate();
         void OnGUI() => Main.Interface.OnGUI();
         void OnDestroy() { if (!IsDestroying) Create(); }
--- Dependencies/SupportModules/Mono/SM_Component.cs
+++ Dependencies/SupportModules/Mono/SM_Component.cs
@@ -7,7 +7,19 @@ namespace MelonLoader.Support
     {
         private static bool IsDestroying = false;
         private static MethodInfo SetAsLastSiblingMethod = null;
-        static SM_Component() { try { SetAsLastSiblingMethod = typeof(Transform).GetMethod("SetAsLastSibling", BindingFlags.Public | BindingFlags.Instance); } catch (System.Exception ex) { MelonLogger.Warning($"Exception while Getting Transform.SetAsLastSibling: {ex}"); } }
+
+        static SM_Component()
+        {
+            try
+            {
+                SetAsLastSiblingMethod = typeof(Transform).GetMethod("SetAsLastSibling", BindingFlags.Public | BindingFlags.Instance);
+            }
+            catch (System.Exception ex)
+            {
+                MelonLogger.Warning($"Exception while Getting Transform.SetAsLastSibling: {ex}");
+            }
+        }
+
         internal static void Create()
         {
             Main.obj = new GameObject();
@@ -16,20 +28,38 @@ namespace MelonLoader.Support
             Main.component = (SM_Component)Main.obj.AddComponent(typeof(SM_Component));
             Main.component.SiblingFix();
         }
-        private void SiblingFix() { SetAsLastSiblingMethod?.Invoke(gameObject.transform, new object[0]); SetAsLastSiblingMethod?.Invoke(transform, new object[0]); }
-        internal void Destroy() { IsDestroying = true; GameObject.Destroy(gameObject); }
-        void Start() { SiblingFix(); Main.Interface.OnApplicationLateStart(); }
+
+        private void SiblingFix()
+        {
+            SetAsLastSiblingMethod?.Invoke(gameObject.transform, new object[0]);
+            SetAsLastSiblingMethod?.Invoke(transform, new object[0]);
+        }
+
+        internal void Destroy()
+        {
+            IsDestroying = true;
+            GameObject.Destroy(gameObject);
+        }
+
+        void Start()
+        {
+            SiblingFix();
+            Main.Interface.OnApplicationLateStart();
+        }
+
         void Awake()
         {
             foreach (var queuedCoroutine in SupportModule_To.QueuedCoroutines)
                 StartCoroutine(queuedCoroutine);
             SupportModule_To.QueuedCoroutines.Clear();
         }
+
         void Update()
         {
             SiblingFix();
             Main.Interface.Update();
         }
+
         void FixedUpdate() => Main.Interface.FixedUpdate();
         void LateUpdate() => Main.Interface.LateUpdate();
         void OnGUI() => Main.Interface.OnGUI();
