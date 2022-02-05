1af1e4f0a260b230d22a23b85f0404cf93649e58
Akiva Silver <akiva.silver@pm.me>
2022-02-05 00:14:23 -0500

    fixed features in sm_il2cpp

--- SM_Il2Cpp/ApplicationHandler.cs
+++ SM_Il2Cpp/ApplicationHandler.cs
@@ -6,6 +6,19 @@ namespace MelonLoader.Support
 {
     internal static class ApplicationHandler
     {
+#if __ANDROID__
+        // internal static string _InternalGUID = new Guid().ToString();
+
+        internal static string GetVersion()
+        {
+            return "0.0.0";
+        }
+
+        internal static string GetBuildGUID()
+        {
+            return "0000";
+        }
+#else
         private static MethodInfo Application_get_version = null;
         private static MethodInfo Application_get_buildGUID = null;
 
@@ -31,5 +44,6 @@ namespace MelonLoader.Support
             => (Application_get_buildGUID != null)
                 ? (string)Application_get_buildGUID.Invoke(null, new object[0])
                 : null;
+#endif
     }
 }
