8ebffcaeec469fa13554c08406003eef7630cf92
SlidyDev <61495410+SlidyDev@users.noreply.github.com>
2022-02-26 15:24:01 +0100

    Cleanup

--- Dependencies/MelonConsoleGUI/Wrappers/WrapUtils.cs
+++ Dependencies/MelonConsoleGUI/Wrappers/WrapUtils.cs
@@ -38,27 +38,7 @@ namespace MelonLoader.Wrappers
             }
         }
 
-        public static unsafe void SetStructArrayElement(object array, int idx, object value)
-        {
-            MelonLogger.Msg(array.GetType().FullName);
-            MelonLogger.Msg(array.GetType().IsGenericType);
-            MelonLogger.Msg(array.GetType().GetGenericArguments()[0].FullName);
-            structArraySetterMethod.Invoke(array, new object[] { idx, value });
-        }
-
         public static object ToIl2CppReferenceArray<T>(this T[] array)
             => Activator.CreateInstance(refArray.MakeGenericType(typeof(T)), new object[] { array });
-
-        public static object ToIl2CppStructArray<T>(this T[] array)
-        {
-            var il2cppArray = Activator.CreateInstance(structArray.MakeGenericType(typeof(T)), new object[] { array.LongLength });
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                SetStructArrayElement(il2cppArray, i, array[i]);
-            }
-
-            return il2cppArray;
-        }
     }
 }
