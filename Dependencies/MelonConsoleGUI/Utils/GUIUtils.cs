9680b162b3ebb8a5ce90ffbc18d82c51c6d77eeb
SlidyDev <61495410+SlidyDev@users.noreply.github.com>
2022-02-26 14:53:36 +0100

    Added a somewhat unstable MelonConsole

---
+++ Dependencies/MelonConsoleGUI/Utils/GUIUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MelonLoader.Console
+{
+    internal static class GUIUtils
+    {
+        public static bool IsFocusedOnConsole => GUI.GetNameOfFocusedControl() == Module.ConsoleFieldName;
+
+        public static bool OnKeyDown(KeyCode key)
+        {
+            var ev = Event.current;
+            return ev.type == EventType.KeyDown && ev.keyCode == key;
+        }
+    }
+}
---
+++ Dependencies/MelonConsoleGUI/Utils/UnityUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MelonLoader.Console
+{
+    internal static class UnityUtils
+    {
+        public static Texture2D CreateColorTexture(Color color)
+        {
+            var tex = new Texture2D(1, 1);
+            tex.SetPixel(0, 0, color);
+            tex.Apply();
+            return tex;
+        }
+
+        public static Color ChangeAlpha(this Color color, float alpha)
+            => new Color(color.r, color.g, color.b, alpha);
+    }
+}
