e65442f4f8adc155a59f70287f9fb8aeaa0aeb7c
SlidyDev <61495410+SlidyDev@users.noreply.github.com>
2022-02-26 16:09:38 +0100

    [MelonConsoleGUI] Added a "proper" check for stripped references.

--- Dependencies/MelonConsoleGUI/Utils/GUIUtils.cs
+++ Dependencies/MelonConsoleGUI/Utils/GUIUtils.cs
@@ -11,5 +11,9 @@ namespace MelonLoader.Console
             var ev = Event.current;
             return ev.type == EventType.KeyDown && ev.keyCode == key;
         }
+
+        // This only exists cuz of stripping. Thx unity
+        public static GUIStyle CopyStyle(GUIStyle style)
+            => new GUIStyle(style);
     }
 }
