55fe9291c75c3dec2689fc1f8e98bab2dba82d6e
ds5678 <jrpritts7@gmail.com>
2022-03-05 04:16:49 -0500

    No IL Repacking (#206)
    * bump AssetRipper.VersionUtilities
    
    * no il repacking
    
    * add forwarding attributes

---
+++ MelonLoader/BackwardsCompatibility/ForwardingAttributes/Mono.Cecil.Pdb.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.NativePdbReader))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.NativePdbWriter))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.NativePdbReaderProvider))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.PdbReaderProvider))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.NativePdbWriterProvider))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Pdb.PdbWriterProvider))]
+[assembly: TypeForwardedTo(typeof(Microsoft.Cci.ILocalScope))]
+[assembly: TypeForwardedTo(typeof(Microsoft.Cci.INamespaceScope))]
+[assembly: TypeForwardedTo(typeof(Microsoft.Cci.IUsedNamespace))]
+[assembly: TypeForwardedTo(typeof(Microsoft.Cci.IName))]
---
+++ MelonLoader/BackwardsCompatibility/ForwardingAttributes/Mono.Cecil.Rocks.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.DocCommentId))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.IILVisitor))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.ILParser))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.MethodBodyRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.MethodDefinitionRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.ModuleDefinitionRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.ParameterReferenceRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.SecurityDeclarationRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.TypeDefinitionRocks))]
+[assembly: TypeForwardedTo(typeof(Mono.Cecil.Rocks.TypeReferenceRocks))]
