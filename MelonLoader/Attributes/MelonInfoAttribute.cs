db69b9ceb8a9b9ee290a2e41bfcde6cc0ee33c09
Herp Derpinstine <herpderpinstine@gmail.com>
2022-10-25 23:26:18 -0500

    Cleanup

--- MelonLoader/Attributes/MelonInfoAttribute.cs
+++ MelonLoader/Attributes/MelonInfoAttribute.cs
@@ -69,9 +69,9 @@ namespace MelonLoader
         /// <param name="type">The main Melon type of the Melon (for example TestMod)</param>
         /// <param name="name">Name of the Melon</param>
         /// <param name="versionMajor">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
-        /// <param name="versionMinor">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
-        /// <param name="versionRevision">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
-        /// <param name="identifier">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
+        /// <param name="versionMinor">Version Minor of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
+        /// <param name="versionRevision">Version Revision of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
+        /// <param name="versionIdentifier">Version Identifier of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
         /// <param name="author">Author of the Melon</param>
         /// <param name="downloadLink">URL to the download link of the mod [optional]</param>
         public MelonInfoAttribute(Type type, string name, int versionMajor, int versionMinor, int versionRevision, string versionIdentifier, string author, string downloadLink = null)
@@ -83,8 +83,8 @@ namespace MelonLoader
         /// <param name="type">The main Melon type of the Melon (for example TestMod)</param>
         /// <param name="name">Name of the Melon</param>
         /// <param name="versionMajor">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
-        /// <param name="versionMinor">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
-        /// <param name="versionRevision">Version Major of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
+        /// <param name="versionMinor">Version Minor of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
+        /// <param name="versionRevision">Version Revision of the Melon (Using the <see href="https://semver.org">Semantic Versioning</see> format)</param>
         /// <param name="author">Author of the Melon</param>
         /// <param name="downloadLink">URL to the download link of the mod [optional]</param>
         public MelonInfoAttribute(Type type, string name, int versionMajor, int versionMinor, int versionRevision, string author, string downloadLink = null)
