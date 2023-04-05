2f2627ee2034cb6b5ecfddb13c7fd031a3d744ad
RinLovesYou <xxklintan123xx@gmail.com>
2023-04-05 14:22:40 +0200

    make args optional

--- Bootstrap/src/melonenv/macros.rs
+++ Bootstrap/src/melonenv/macros.rs
@@ -4,7 +4,7 @@ macro_rules! debug_enabled {
         if cfg!(debug_assertions) {
             true
         } else {
-            $crate::melonenv::args::ARGS.debug
+            $crate::melonenv::args::ARGS.debug.is_some_and(|b| b)
         }
     };
 }
@@ -12,20 +12,20 @@ macro_rules! debug_enabled {
 #[macro_export]
 macro_rules! should_set_title {
     () => {
-        !$crate::melonenv::args::ARGS.console_dst
+        !$crate::melonenv::args::ARGS.console_dst.is_some_and(|b| b)
     };
 }
 
 #[macro_export]
 macro_rules! console_on_top {
     () => {
-        $crate::melonenv::args::ARGS.console_on_top
+        $crate::melonenv::args::ARGS.console_on_top.is_some_and(|b| b)
     };
 }
 
 #[macro_export]
 macro_rules! hide_console {
     () => {
-        $crate::melonenv::args::ARGS.hide_console
+        $crate::melonenv::args::ARGS.hide_console.is_some_and(|b| b)
     };
 }
