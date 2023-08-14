42f943deebcdd08ffc9fcc55d927799744765678
Sarah <xxklintan123xx@gmail.com>
2023-08-14 23:51:14 +0200

    Make Preload cross-platform

--- Dependencies/SupportModules/Preload/Preload.cs
+++ Dependencies/SupportModules/Preload/Preload.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
@@ -8,23 +9,32 @@ namespace MelonLoader.Support
     {
         private static void Initialize()
         {
-            string ManagedFolder = string.Copy(GetManagedDirectory());
+            string managedFolder = string.Copy(GetManagedDirectory());
 
-            string SystemPath = Path.Combine(ManagedFolder, "System.dll");
-            if (!File.Exists(SystemPath))
-                File.WriteAllBytes(SystemPath, Properties.Resources.System);
+            string systemPath = Path.Combine(managedFolder, "System.dll");
+            if (!File.Exists(systemPath))
+                WriteResource("System.dll", systemPath);
 
-            string SystemCorePath = Path.Combine(ManagedFolder, "System.Core.dll");
-            if (!File.Exists(SystemCorePath))
-                File.WriteAllBytes(SystemCorePath, Properties.Resources.System_Core);
+            string systemCorePath = Path.Combine(managedFolder, "System.Core.dll");
+            if (!File.Exists(systemCorePath))
+                WriteResource("System.Core.dll", systemCorePath);
 
-            string SystemDrawingPath = Path.Combine(ManagedFolder, "System.Drawing.dll");
-            if (!File.Exists(SystemDrawingPath))
-                File.WriteAllBytes(SystemDrawingPath, Properties.Resources.System_Drawing);
+            string systemDrawingPath = Path.Combine(managedFolder, "System.Drawing.dll");
+            if (!File.Exists(systemDrawingPath))
+                WriteResource("System.Drawing.dll", systemCorePath);
         }
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         [return: MarshalAs(UnmanagedType.LPStr)]
-        private extern static string GetManagedDirectory();
+        private static extern string GetManagedDirectory();
+
+        private static void WriteResource(string name, string destination)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+            var fileStream = new FileStream(destination, FileMode.CreateNew);
+            for (int i = 0; i < stream!.Length; i++)
+                fileStream.WriteByte((byte)stream.ReadByte());
+            fileStream.Close();
+        }
     }
 }
