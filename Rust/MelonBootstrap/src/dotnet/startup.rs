1cb8876f17d38cb964bf54a69eeeedc9ba0b7f6d
Sarah <xxklintan123xx@gmail.com>
2023-11-21 16:15:41 +0100

    switch to portable dotnet install

--- Rust/MelonBootstrap/src/dotnet/startup.rs
+++ Rust/MelonBootstrap/src/dotnet/startup.rs
@@ -1,8 +1,19 @@
-use std::{error::Error, ffi::c_void, sync::{LazyLock, Mutex}, ptr::{addr_of_mut, null_mut, addr_of}};
+use std::{
+    error::Error,
+    ffi::c_void,
+    ptr::{addr_of, addr_of_mut, null_mut},
+    sync::{LazyLock, Mutex},
+};
 
-use netcorehost::{nethost::load_hostfxr, pdcstr};
+use netcorehost::{
+    nethost::{load_hostfxr, load_hostfxr_with_dotnet_root},
+    pdcstr,
+};
 
-use crate::{environment, utils::{self, strings::wide_str}, debug, icalls};
+use crate::{
+    debug, environment, icalls,
+    utils::{self, strings::wide_str},
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -19,13 +30,21 @@ pub struct HostExports {
     pub hook_detach: unsafe fn(*mut c_void),
 }
 
-pub static IMPORTS: LazyLock<Mutex<HostImports>> = LazyLock::new(|| Mutex::new(HostImports {
-    load_assembly_get_ptr: |_, _, _, _| {},
-    initialize: || {},
-}));
+pub static IMPORTS: LazyLock<Mutex<HostImports>> = LazyLock::new(|| {
+    Mutex::new(HostImports {
+        load_assembly_get_ptr: |_, _, _, _| {},
+        initialize: || {},
+    })
+});
 
 pub fn start() -> Result<(), Box<dyn Error>> {
-    let hostfxr = load_hostfxr()?;
+    let hostfxr = load_hostfxr_with_dotnet_root(utils::strings::pdcstr(
+        environment::paths::get_base_dir()?
+            .join("MelonLoader")
+            .join("Dependencies")
+            .join("dotnet"),
+    )?)?;
+    
     let runtime_dir = environment::paths::get_base_dir()?
         .join("MelonLoader")
         .join("net6");
@@ -46,53 +65,55 @@ pub fn start() -> Result<(), Box<dyn Error>> {
     let loader =
         context.get_delegate_loader_for_assembly(utils::strings::pdcstr(bootstrap_path)?)?;
 
-        let init = loader.get_function_with_unmanaged_callers_only::<fn(*mut HostImports)>(
-            pdcstr!("MelonLoader.NativeHost.NativeEntryPoint, MelonLoader.NativeHost"),
-            pdcstr!("LoadStage1"),
-        )?;
-    
-        let mut imports = HostImports {
-            load_assembly_get_ptr: |_, _, _, _| {},
-            initialize: || {},
-        };
-    
-        let mut exports = HostExports {
-            hook_attach: icalls::bootstrap_interop::attach,
-            hook_detach: icalls::bootstrap_interop::detach,
-        };
-    
-        debug!("[Dotnet] Invoking LoadStage1")?;
-        //MelonLoader.NativeHost will fill in the HostImports struct with pointers to functions
-        init(addr_of_mut!(imports));
-    
-        debug!("[Dotnet] Reloading NativeHost into correct load context and getting LoadStage2 pointer")?;
-    
-        //a function pointer to be filled
-        let mut init_stage_two = null_mut::<c_void>();
-    
-        //have to make all strings utf16 for C# to understand, of course they can only be passed as IntPtrs
-        (imports.load_assembly_get_ptr)(
-            wide_str(runtime_dir.join("MelonLoader.NativeHost.dll"))?.as_ptr() as isize,
-            wide_str("MelonLoader.NativeHost.NativeEntryPoint, MelonLoader.NativeHost")?.as_ptr()
-                as isize,
-            wide_str("LoadStage2")?.as_ptr() as isize,
-            addr_of_mut!(init_stage_two),
-        );
-    
-        debug!("[Dotnet] Invoking LoadStage2")?;
-    
-        //turn the function pointer into a function we can invoke
-        let init_stage_two: fn(*mut HostImports, *mut HostExports) =
-            unsafe { std::mem::transmute(init_stage_two) };
-        init_stage_two(addr_of_mut!(imports), addr_of_mut!(exports));
-    
-        if addr_of!(imports.initialize).is_null() {
-            Err("Failed to get HostImports::Initialize!")?
-        }
-    
-        (imports.initialize)();
-    
-        *IMPORTS.try_lock()? = imports;
-    
-        Ok(())
+    let init = loader.get_function_with_unmanaged_callers_only::<fn(*mut HostImports)>(
+        pdcstr!("MelonLoader.NativeHost.NativeEntryPoint, MelonLoader.NativeHost"),
+        pdcstr!("LoadStage1"),
+    )?;
+
+    let mut imports = HostImports {
+        load_assembly_get_ptr: |_, _, _, _| {},
+        initialize: || {},
+    };
+
+    let mut exports = HostExports {
+        hook_attach: icalls::bootstrap_interop::attach,
+        hook_detach: icalls::bootstrap_interop::detach,
+    };
+
+    debug!("[Dotnet] Invoking LoadStage1")?;
+    //MelonLoader.NativeHost will fill in the HostImports struct with pointers to functions
+    init(addr_of_mut!(imports));
+
+    debug!(
+        "[Dotnet] Reloading NativeHost into correct load context and getting LoadStage2 pointer"
+    )?;
+
+    //a function pointer to be filled
+    let mut init_stage_two = null_mut::<c_void>();
+
+    //have to make all strings utf16 for C# to understand, of course they can only be passed as IntPtrs
+    (imports.load_assembly_get_ptr)(
+        wide_str(runtime_dir.join("MelonLoader.NativeHost.dll"))?.as_ptr() as isize,
+        wide_str("MelonLoader.NativeHost.NativeEntryPoint, MelonLoader.NativeHost")?.as_ptr()
+            as isize,
+        wide_str("LoadStage2")?.as_ptr() as isize,
+        addr_of_mut!(init_stage_two),
+    );
+
+    debug!("[Dotnet] Invoking LoadStage2")?;
+
+    //turn the function pointer into a function we can invoke
+    let init_stage_two: fn(*mut HostImports, *mut HostExports) =
+        unsafe { std::mem::transmute(init_stage_two) };
+    init_stage_two(addr_of_mut!(imports), addr_of_mut!(exports));
+
+    if addr_of!(imports.initialize).is_null() {
+        Err("Failed to get HostImports::Initialize!")?
+    }
+
+    (imports.initialize)();
+
+    *IMPORTS.try_lock()? = imports;
+
+    Ok(())
 }
