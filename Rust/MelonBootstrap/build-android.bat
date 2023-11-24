caf161304e1dfa96241deb8a621ab8fd8523ce40
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-11-24 16:18:35 -0500

    Remove some debugging stuff

--- Rust/MelonBootstrap/build-android.bat
+++ Rust/MelonBootstrap/build-android.bat
@@ -1,8 +1,6 @@
 @echo off
 cargo ndk -t arm64-v8a -o ./jniLibs build
-rem --release
-copy "%~dp0jniLibs\arm64-v8a\libmelon_bootstrap.so" "C:/Users/trevo/Desktop/android_universe/lib/arm64-v8a"
-copy "%~dp0jniLibs\arm64-v8a\libmain.so" "C:/Users/trevo/Desktop/android_universe/lib/arm64-v8a"
+rem add --release to build as release
 IF "%1" NEQ "auto" (
     pause
 )
