147bcdbc7f2774c78240d1e2808dbea0b1f97288
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-11-24 13:42:01 -0500

    Add Android proxy project

---
+++ Rust/MelonAndroidProxy/src/nativeloader.rs
@@ -0,0 +1,68 @@
+use jni::{
+    objects::{JClass, JString},
+    sys::{jboolean, JavaVM},
+    JNIEnv,
+};
+use std::path::PathBuf;
+use std::ffi::CStr;
+
+use crate::utils::{libs::load_lib, self};
+
+#[no_mangle]
+fn load(env: JNIEnv, _: JClass, _: JString) -> jboolean {
+    load_bootstrap(&env);
+    load_lib_unity(&env);
+    return 1;
+}
+
+#[no_mangle]
+fn unload(_: JNIEnv, _: JClass) {
+    info!("unload");
+}
+
+fn load_bootstrap(env: &JNIEnv) {
+    let bootstrap_lib = load_lib(&PathBuf::from("libmelon_bootstrap.so"), libc::RTLD_LAZY)
+        .unwrap_or_else(|e| {
+            error!("Failed to load libmelon_bootstrap.so: {}", e.to_string());
+
+            let dl_error = unsafe { libc::dlerror() };
+            let error_message = unsafe {
+                CStr::from_ptr(dl_error)
+            };
+            let formatted_string = error_message.to_string_lossy();
+            error!("dlerror: {}", formatted_string);
+            panic!();
+        });
+
+    let on_load: utils::libs::NativeMethod<fn(*mut JavaVM, *mut libc::c_void)> = bootstrap_lib
+        .sym("JNI_OnLoad")
+        .unwrap_or_else(|e| {
+            error!("Failed to find JNI_OnLoad: {}", e.to_string());
+            panic!();
+        });
+
+    (on_load)(env.get_java_vm().unwrap().get_java_vm_pointer(), std::ptr::null_mut());
+
+    let initialize: utils::libs::NativeMethod<fn()> = bootstrap_lib
+        .sym("startup")
+        .unwrap_or_else(|e| {
+            info!("Failed to find Initialize: {}", e.to_string());
+            panic!();
+        });
+        
+    (initialize)();
+}
+
+fn load_lib_unity(env: &JNIEnv) {
+    let unity_lib = load_lib(&PathBuf::from("libunity.so"), libc::RTLD_NOW | libc::RTLD_GLOBAL)
+        .expect("Couldn't load libunity!");
+
+    let on_load: utils::libs::NativeMethod<fn(*mut JavaVM, *mut libc::c_void)> = unity_lib
+        .sym("JNI_OnLoad")
+        .expect("Couldn't find JNI_OnLoad!");
+
+    (on_load)(
+        env.get_java_vm().expect("Failed to get JavaVM from JNIEnv").get_java_vm_pointer(),
+        std::ptr::null_mut()
+    );
+}
---
+++ Rust/MelonAndroidProxy/src/utils/libs.rs
@@ -0,0 +1,257 @@
+//! cross platform utilities for permanently loading libraries
+//!
+//! Main use case here is just permanently having mono/dobby/etc loaded, unlike with libloading,
+//! where they get unloaded when the library goes out of scope.
+//!
+//! # Safety
+//! this is incredibly unsafe
+//!
+//! # Examples
+//!
+//! ```
+//! use std::path::PathBuf;
+//! use std::mem;
+//!
+//! use unity_rs::libs::load_lib;
+//!
+//! let path = PathBuf::from("path/to/lib");
+//! let lib = load_lib(&path)?;
+//!
+//! let func: extern fn() = unsafe { mem::transmute(lib.get_fn_ptr("func_name")?) };
+
+use std::{
+    ffi::{
+        c_void,
+        c_int
+    },
+    marker::PhantomData,
+    ops::Deref,
+    path::{Path, PathBuf},
+};
+use thiserror::Error;
+
+/// possible library loading errors
+#[derive(Debug, Error)]
+pub enum LibError {
+    /// failed to load library
+    #[error("Failed to load library!")]
+    FailedToLoadLib,
+
+    /// failed to get lib name
+    #[error("Failed to get lib name!")]
+    FailedToGetLibName,
+
+    /// failed to get lib path
+    #[error("Failed to get lib path!")]
+    FailedToGetLibPath,
+
+    /// failed to get function pointer
+    #[error("Failed to get function pointer: {0}")]
+    FailedToGetFnPtr(String),
+
+    #[error("Failed to create C-String")]
+    FailedToCreateCString,
+}
+
+/// a representation of a permanently loaded library
+#[derive(Debug, Clone)]
+pub struct NativeLibrary {
+    /// the name of the lib
+    pub name: String,
+    /// the path to the lib
+    pub path: PathBuf,
+    /// the pointer to the lib
+    pub handle: *mut c_void,
+}
+
+impl NativeLibrary {
+    /// gets a function pointer
+    #[cfg(not(target_os = "windows"))]
+    pub fn sym<T>(&self, name_str: &str) -> Result<NativeMethod<T>, LibError> {
+        let display_string = name_str.to_string();
+
+        let name = std::ffi::CString::new(name_str).map_err(|_| LibError::FailedToCreateCString)?;
+        let ptr = unsafe { libc::dlsym(self.handle, name.as_ptr()) };
+        if ptr.is_null() {
+            return Err(LibError::FailedToGetFnPtr(display_string));
+        }
+
+        Ok(NativeMethod {
+            inner: ptr.cast(),
+            pd: PhantomData,
+        })
+    }
+
+    /// gets a function pointer
+    #[cfg(target_os = "windows")]
+    pub fn sym<T>(&self, name_str: &str) -> Result<NativeMethod<T>, LibError> {
+        use std::ffi::CString;
+
+        let display_string = name_str.to_string();
+
+        use winapi::um::libloaderapi::GetProcAddress;
+
+        let name = CString::new(name_str).map_err(|_| LibError::FailedToCreateCString)?;
+
+        let ptr = unsafe { GetProcAddress(self.handle.cast(), name.as_ptr()) };
+        if ptr.is_null() {
+            return Err(LibError::FailedToGetFnPtr(display_string));
+        }
+
+        Ok(NativeMethod {
+            inner: ptr.cast(),
+            pd: PhantomData,
+        })
+    }
+}
+
+/// loads a library permanently
+///
+/// # Arguments
+///
+/// * `path` - the path to the library
+///
+/// # Errors
+///
+/// * `LibError::FailedToLoadLib` - if the library failed to load
+/// * `LibError::FailedToGetLibName` - if the library name failed to be retrieved
+///
+/// # Safety
+///
+/// this function is unsafe because it permanently loads a library
+///
+/// # Examples
+///
+/// ```
+/// use std::path::PathBuf;
+///
+/// use unity_rs::libs::load_lib;
+///
+/// let path = PathBuf::from("path/to/lib");
+/// let lib = load_lib(&path);
+///
+/// assert!(lib.is_ok());
+#[cfg(not(target_os = "windows"))]
+pub fn load_lib<P: AsRef<Path>>(path: P, rtld: c_int) -> Result<NativeLibrary, LibError> {
+    use std::ffi::{CString};
+
+    let path = path.as_ref();
+
+    let path_string = path.to_str().ok_or(LibError::FailedToGetLibPath)?;
+
+    let c_path = CString::new(path_string).map_err(|_| LibError::FailedToCreateCString)?;
+
+    let lib = unsafe { libc::dlopen(c_path.as_ptr(), rtld) };
+
+    if lib.is_null() {
+        return Err(LibError::FailedToLoadLib);
+    }
+
+    let lib_name = path
+        .file_name()
+        .ok_or(LibError::FailedToGetLibName)?
+        .to_str()
+        .ok_or(LibError::FailedToGetLibName)?
+        .to_string();
+
+    Ok(NativeLibrary {
+        name: lib_name,
+        path: path.to_path_buf(),
+        handle: lib,
+    })
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn load_self() -> Result<NativeLibrary, LibError> {
+    let lib = unsafe { libc::dlopen(std::ptr::null(), libc::RTLD_NOW | libc::RTLD_GLOBAL) };
+
+    if lib.is_null() {
+        return Err(LibError::FailedToLoadLib);
+    }
+
+    Ok(NativeLibrary {
+        name: "".into(),
+        path: "".into(),
+        handle: lib,
+    })
+}
+
+/// loads a library permanently
+///
+/// # Arguments
+///
+/// * `path` - the path to the library
+///
+/// # Errors
+///
+/// * `LibError::FailedToLoadLib` - if the library failed to load
+/// * `LibError::FailedToGetLibName` - if the library name failed to be retrieved
+///
+/// # Safety
+///
+/// this function is unsafe because it permanently loads a library
+///
+/// # Examples
+///
+/// ```
+/// use std::path::PathBuf;
+///
+/// use unity_rs::libs::load_lib;
+///
+/// let path = PathBuf::from("path/to/lib");
+/// let lib = load_lib(&path);
+///
+/// assert!(lib.is_ok());
+#[cfg(target_os = "windows")]
+pub fn load_lib<P: AsRef<Path>>(path: P) -> Result<NativeLibrary, LibError> {
+    use std::ffi::CString;
+
+    let path = path.as_ref();
+
+    use winapi::um::libloaderapi::LoadLibraryA;
+
+    let path_string = path.to_str().ok_or_else(|| LibError::FailedToGetLibPath)?;
+    let win_path = CString::new(path_string).map_err(|_| LibError::FailedToCreateCString)?;
+
+    let lib = unsafe { LoadLibraryA(win_path.as_ptr()) };
+
+    if lib.is_null() {
+        return Err(LibError::FailedToLoadLib);
+    }
+
+    let lib_name = path
+        .file_name()
+        .ok_or(LibError::FailedToGetLibName)?
+        .to_str()
+        .ok_or(LibError::FailedToGetLibName)?
+        .to_string();
+
+    Ok(NativeLibrary {
+        name: lib_name,
+        path: path.to_path_buf(),
+        handle: lib.cast(),
+    })
+}
+
+#[derive(Debug)]
+pub struct NativeMethod<T> {
+    pub inner: *mut c_void,
+    pd: PhantomData<T>,
+}
+
+unsafe impl<T: Send> Send for NativeMethod<T> {}
+unsafe impl<T: Sync> Sync for NativeMethod<T> {}
+
+impl<T> Clone for NativeMethod<T> {
+    fn clone(&self) -> NativeMethod<T> {
+        NativeMethod { ..*self }
+    }
+}
+
+impl<T> Deref for NativeMethod<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*(&self.inner as *const *mut _ as *const T) }
+    }
+}
