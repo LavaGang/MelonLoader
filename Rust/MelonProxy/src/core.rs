b9d36ee0db456183b0c4821f2984b8627332318b
Sarah <xxklintan123xx@gmail.com>
2023-12-06 01:15:18 +0100

    Ignore UnityCrashHandler

--- Rust/MelonProxy/src/core.rs
+++ Rust/MelonProxy/src/core.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, sync::{LazyLock, Mutex}, path::PathBuf};
+use std::{
+    error::Error,
+    path::PathBuf,
+    sync::{LazyLock, Mutex},
+};
 
 use libloading::Library;
 
@@ -12,6 +16,18 @@ pub fn init() -> Result<(), Box<dyn Error>> {
     //TODO: Support UTF-16 (it will suck)
     let mut base_dir = std::env::current_dir()?;
     let mut no_mods = false;
+
+    let current_exe = std::env::current_exe()?;
+    let game_name = current_exe
+        .file_name()
+        .ok_or("Failed to get game name")?
+        .to_str()
+        .ok_or("Failed to get game name")?;
+
+    if game_name.starts_with("UnityCrashHandler64") || game_name.starts_with("UnityCrashHandler32")
+    {
+        return Ok(());
+    }
 
     for arg in args.iter() {
         if arg.starts_with("--melonloader.basedir") {
