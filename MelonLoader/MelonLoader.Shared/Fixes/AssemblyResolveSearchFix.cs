4f671d448ffbbcb91035a1e4a0b058697f32f503
Herp Derpinstine <herpderpinstine@gmail.com>
2023-12-06 00:23:20 -0600

    Implemented Assembly Resolver Fix for Mono

--- MelonLoader/MelonLoader.Shared/Fixes/AssemblyResolveSearchFix.cs
+++ MelonLoader/MelonLoader.Shared/Fixes/AssemblyResolveSearchFix.cs
@@ -1,61 +1,61 @@
 using MelonLoader.Utils;
-
-#if NET6_0
 using System.IO;
 using System.Reflection;
+
+#if NET6_0
 using System.Runtime.Loader;
+#else
+using System;
 #endif
 
 namespace MelonLoader.Fixes
 {
     public static class AssemblyResolveSearchFix
     {
-        private static string[] SearchableDirectories;
+#if NET6_0
+        private static AssemblyLoadContext _alc;
+#endif
+
+        private static string[] SearchableDirectories = new string[]
+        {
+#if NET6_0
+            Path.Combine(MelonEnvironment.ModulesDirectory, "Mono", "net6"),
+#elif NET35
+            Path.Combine(Path.Combine(MelonEnvironment.ModulesDirectory, "Mono"), "net35"),
+#else
+            Path.Combine(MelonEnvironment.ModulesDirectory, "Mono", "netstandard2.1"),
+#endif
+            MelonEnvironment.ModulesDirectory,
+            MelonEnvironment.UserLibsDirectory,
+            MelonEnvironment.PluginsDirectory,
+            MelonEnvironment.ModsDirectory,
+            MelonEnvironment.MelonBaseDirectory,
+            MelonEnvironment.GameRootDirectory
+        };
 
         public static void Install()
         {
 #if NET6_0
-            SearchableDirectories = new string[]
-            {
-                Path.Combine(MelonEnvironment.ModulesDirectory, "Mono", "net6"),
-                MelonEnvironment.ModulesDirectory,
-                MelonEnvironment.UserLibsDirectory,
-                MelonEnvironment.PluginsDirectory,
-                MelonEnvironment.ModsDirectory,
-                MelonEnvironment.MelonBaseDirectory,
-                MelonEnvironment.GameRootDirectory
-            };
-
             AssemblyLoadContext.Default.Resolving += OnResolve;
+#else
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolve;
 #endif
         }
 
-#if NET6_0
-        private static Assembly TryLoad(AssemblyLoadContext alc, string path)
+        private static Assembly FindAssembly(string name)
         {
-            if (File.Exists(path))
-            {
-                MelonDebug.Msg($"[{nameof(AssemblyResolveSearchFix)}] Loading from {path}...");
-                return alc.LoadFromAssemblyPath(path);
-            }
-
-            return null;
-        }
-
-        private static Assembly OnResolve(AssemblyLoadContext alc, AssemblyName name)
-        {
-            var filename = name.Name + ".dll";
+            var filename = name + ".dll";
 
             Assembly ret = null;
             foreach (string folder in SearchableDirectories)
             {
-                ret = TryLoad(alc, Path.Combine(folder, filename));
+                ret = TryLoad(Path.Combine(folder, filename));
                 if (ret != null)
                     return ret;
 
                 foreach (var childFolder in Directory.GetDirectories(folder))
                 {
-                    ret = TryLoad(alc, Path.Combine(childFolder, filename));
+                    ret = TryLoad(Path.Combine(childFolder, filename));
                     if (ret != null)
                         return ret;
                 }
@@ -65,6 +65,31 @@ namespace MelonLoader.Fixes
                 MelonDebug.Msg($"[{nameof(AssemblyResolveSearchFix)}] Failed to find {filename} in any of the known search directories");
             return ret;
         }
+
+        private static Assembly TryLoad(string path)
+        {
+            if (File.Exists(path))
+            {
+                MelonDebug.Msg($"[{nameof(AssemblyResolveSearchFix)}] Loading from {path}...");
+#if NET6_0
+                return _alc.LoadFromAssemblyPath(path);
+#else
+                return Assembly.LoadFrom(path);
+#endif
+            }
+
+            return null;
+        }
+
+#if NET6_0
+        private static Assembly OnResolve(AssemblyLoadContext alc, AssemblyName name)
+        {
+            _alc = alc;
+            return FindAssembly(name.Name);
+        }
+#else
+        private static Assembly OnResolve(object sender, ResolveEventArgs args)
+            => FindAssembly(args.Name);
 #endif
     }
 }
