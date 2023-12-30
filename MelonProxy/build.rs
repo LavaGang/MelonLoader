61a6da5e3651eefda8ef6e5224d4cb6dc9336211
Sarah <xxklintan123xx@gmail.com>
2023-12-30 20:00:08 +0100

    Try fixing build script again

--- MelonProxy/build.rs
+++ MelonProxy/build.rs
@@ -4,29 +4,25 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS");
 
     match target_os.as_ref().map(|x| &**x) {
-        Ok("linux") | Ok("android") => {},
-        Ok("freebsd") | Ok("dragonfly") => {},
+        Ok("linux") | Ok("android") => {}
+        Ok("freebsd") | Ok("dragonfly") => {}
         Ok("openbsd") | Ok("bitrig") | Ok("netbsd") | Ok("macos") | Ok("ios") => {}
 
         Ok("windows") => link_exports(),
 
-        tos => panic!("unknown target os {:?}!", tos)
+        tos => panic!("unknown target os {:?}!", tos),
     }
 }
 
 /// links Exports.def to the resulting dll, exporting all our asm functions.
 fn link_exports() {
-    use std::path::Path;
-    let lib_path = Path::new("deps").join("Exports.def");
-    let absolute_path = std::fs::canonicalize(&lib_path).unwrap().to_str().unwrap().to_string().replace("\\\\?\\", "");
+    let lib_path = env::current_dir().unwrap().join("deps").join("Exports.def");
 
-    if !Path::new(&absolute_path).exists() {
-        panic!("Exports.def not found at {}", lib_path.display());
+
+    if !lib_path.exists() {
+        println!("cargo:error=Exports.def not found at {}", lib_path.display());
     }
 
-    println!("cargo:warning=Linking Exports File: {}", absolute_path);
-    println!(
-        "cargo:rustc-cdylib-link-arg=/DEF:{}",
-        absolute_path
-    );
+    println!("cargo:warning=Linking Exports File: {}", lib_path.display());
+    println!("cargo:rustc-cdylib-link-arg=/DEF:{}", lib_path.display());
 }
