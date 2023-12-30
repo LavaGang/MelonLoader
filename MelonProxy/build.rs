9181d4c510982977d9ba90957da6157cf3d5b3cf
Sarah <xxklintan123xx@gmail.com>
2023-12-30 19:22:41 +0100

    Fix path issue in MelonProxy build.rs

--- MelonProxy/build.rs
+++ MelonProxy/build.rs
@@ -18,10 +18,18 @@ fn main() {
 fn link_exports() {
     use std::path::Path;
     let lib_path = Path::new("deps").join("Exports.def");
-    let absolute_path = std::fs::canonicalize(&lib_path).unwrap();
-    println!("cargo:warning=Linking Exports File: {}", absolute_path.display());
+    let mut absolute_path = std::fs::canonicalize(&lib_path).unwrap().to_str().unwrap().to_string();
+    if absolute_path.contains("\\\\?\\") {
+        absolute_path = absolute_path.replace("\\\\?\\", "");
+    }
+
+    if !lib_path.exists() {
+        panic!("Exports.def not found at {}", lib_path.display());
+    }
+
+    println!("cargo:warning=Linking Exports File: {}", absolute_path);
     println!(
         "cargo:rustc-cdylib-link-arg=/DEF:{}",
-        absolute_path.display()
+        absolute_path
     );
 }
