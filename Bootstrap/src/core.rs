c2baf5d3dc20d14562f2abc8e81b0a121637be89
Luis Scheurenbrand <luis@scheurenbrand.me>
2024-03-03 21:28:59 +0100

    don't abort game launch on MelonLoader initialization failure

--- Bootstrap/src/core.rs
+++ Bootstrap/src/core.rs
@@ -5,7 +5,7 @@ use crate::{console, errors::DynErr, hooks, internal_failure, logging::logger};
 #[ctor]
 fn startup() {
     init().unwrap_or_else(|e| {
-        internal_failure!("Failed to initialize MelonLoader: {}", e.to_string());
+        eprintln!("Failed to initialize MelonLoader: {}", e.to_string());
     })
 }
 
