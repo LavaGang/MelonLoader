b823e64b5cf59d855f71d99f976c3cc0e2b0ea15
Herp Derpinstine <herpderpinstine@gmail.com>
2024-06-24 19:12:44 -0600

    Cpp2IL 2022.1.0-pre-release.11

--- Dependencies/Il2CppAssemblyGenerator/Packages/Cpp2IL.cs
+++ Dependencies/Il2CppAssemblyGenerator/Packages/Cpp2IL.cs
@@ -17,7 +17,7 @@ namespace MelonLoader.Il2CppAssemblyGenerator.Packages
                 Version = RemoteAPI.Info.ForceDumperVersion;
 #endif
             if (string.IsNullOrEmpty(Version) || Version.Equals("0.0.0.0"))
-                Version = "2022.1.0-pre-release.10";
+                Version = $"2022.1.0-pre-release.{(MelonUtils.IsWindows ? ".11" : ".10")}";
 
             Name = nameof(Cpp2IL);
             Destination = Path.Combine(Core.BasePath, Name);
