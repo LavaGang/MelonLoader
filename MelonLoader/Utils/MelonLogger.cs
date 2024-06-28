6a1d68bf39bbd36c0d29be5fe366f1df2a0d2ffb
Herp Derpinstine <herpderpinstine@gmail.com>
2024-06-27 19:52:49 -0600

    Fix Package References

--- MelonLoader/Utils/MelonLogger.cs
+++ MelonLoader/Utils/MelonLogger.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Text;
 using static MelonLoader.Utils.LoggerUtils;
 using System.Collections.Generic;
+using ValueTuple = System.ValueTuple;
 
 namespace MelonLoader
 {
