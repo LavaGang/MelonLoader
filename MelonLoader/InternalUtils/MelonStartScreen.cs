bc67213524c4f27566f80247c199189174416fdd
Herp Derpinstine <herpderpinstine@gmail.com>
2024-09-26 13:25:22 -0600

    Temporarily removed Start Screen for being broken in most cases
    Will make a return later.
    This is just currently causing more issues than its worth.

--- MelonLoader/InternalUtils/MelonStartScreen.cs
+++ MelonLoader/InternalUtils/MelonStartScreen.cs
@@ -1,7 +1,7 @@
-﻿using System.IO;
-using AssetRipper.VersionUtilities;
-using MelonLoader.Modules;
-using MelonLoader.Utils;
+﻿//using System.IO;
+//using AssetRipper.VersionUtilities;
+//using MelonLoader.Modules;
+//using MelonLoader.Utils;
 
 namespace MelonLoader.InternalUtils
 {
@@ -10,20 +10,20 @@ namespace MelonLoader.InternalUtils
         // Doesn't support Unity versions lower than 2017.2.0 (yet?)
         // Doesn't support Unity versions lower than 2018 (Crashing Issue)
         // Doesn't support Unity versions higher than to 2020.3.21 (Crashing Issue)
-        internal static readonly MelonModule.Info moduleInfo = new MelonModule.Info($"{MelonEnvironment.OurRuntimeDirectory}{Path.DirectorySeparatorChar}MelonStartScreen.dll"
-            , () => !MelonLaunchOptions.Core.StartScreen || UnityInformationHandler.EngineVersion < new UnityVersion(2018));
+        //internal static readonly MelonModule.Info moduleInfo = new MelonModule.Info($"{MelonEnvironment.OurRuntimeDirectory}{Path.DirectorySeparatorChar}MelonStartScreen.dll"
+        //    , () => !MelonLaunchOptions.Core.StartScreen || UnityInformationHandler.EngineVersion < new UnityVersion(2018));
 
         internal static int LoadAndRun(LemonFunc<int> functionToWaitForAsync)
         {
-            var module = MelonModule.Load(moduleInfo);
-            if (module == null)
+            //var module = MelonModule.Load(moduleInfo);
+            //if (module == null)
                 return functionToWaitForAsync();
 
-            var result = module.SendMessage("LoadAndRun", functionToWaitForAsync);
-            if (result is int resultCode)
-                return resultCode;
+            //var result = module.SendMessage("LoadAndRun", functionToWaitForAsync);
+            //if (result is int resultCode)
+            //    return resultCode;
 
-            return -1;
+            //return -1;
         }
     }
 }
