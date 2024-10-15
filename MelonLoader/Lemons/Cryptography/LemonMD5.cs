77889af4eabc44390c77a90c8e459551a7ba5cda
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-14 18:38:46 -0600

    Fixed Regression with LemonMD5, LemonSHA256, and LemonSHA512

--- MelonLoader/Lemons/Cryptography/LemonMD5.cs
+++ MelonLoader/Lemons/Cryptography/LemonMD5.cs
@@ -6,14 +6,19 @@ namespace MelonLoader.Lemons.Cryptography
 	public class LemonMD5
 	{
 		private HashAlgorithm algorithm;
+        private static LemonMD5 static_algorithm = new();
 
-		public LemonMD5()
+        public LemonMD5()
 		{
 			algorithm = (HashAlgorithm)CryptoConfig.CreateFromName("System.Security.Cryptography.MD5");
 			algorithm.SetHashSizeValue(256);
 		}
 
-		public byte[] ComputeHash(byte[] buffer) => algorithm.ComputeHash(buffer);
+        public static byte[] ComputeMD5Hash(byte[] buffer) => static_algorithm.ComputeHash(buffer);
+        public static byte[] ComputeMD5Hash(byte[] buffer, int offset, int count) => static_algorithm.ComputeHash(buffer, offset, count);
+        public static byte[] ComputeMD5Hash(Stream inputStream) => static_algorithm.ComputeHash(inputStream);
+
+        public byte[] ComputeHash(byte[] buffer) => algorithm.ComputeHash(buffer);
 		public byte[] ComputeHash(byte[] buffer, int offset, int count) => algorithm.ComputeHash(buffer, offset, count);
 		public byte[] ComputeHash(Stream inputStream) => algorithm.ComputeHash(inputStream);
 	}
--- MelonLoader/Lemons/Cryptography/LemonSHA512.cs
+++ MelonLoader/Lemons/Cryptography/LemonSHA512.cs
@@ -6,12 +6,17 @@ namespace MelonLoader.Lemons.Cryptography
 	public class LemonSHA512
 	{
 		private HashAlgorithm algorithm;
+        private static LemonSHA512 static_algorithm = new();
 
-		public LemonSHA512()
+        public LemonSHA512()
 		{
 			algorithm = (HashAlgorithm)CryptoConfig.CreateFromName("System.Security.Cryptography.SHA512");
 			algorithm.SetHashSizeValue(512);
-		}
+        }
+
+        public static byte[] ComputeSHA512Hash(byte[] buffer) => static_algorithm.ComputeHash(buffer);
+        public static byte[] ComputeSHA512Hash(byte[] buffer, int offset, int count) => static_algorithm.ComputeHash(buffer, offset, count);
+        public static byte[] ComputeSHA512Hash(Stream inputStream) => static_algorithm.ComputeHash(inputStream);
 
         public byte[] ComputeHash(byte[] buffer) => algorithm.ComputeHash(buffer);
 		public byte[] ComputeHash(byte[] buffer, int offset, int count) => algorithm.ComputeHash(buffer, offset, count);
