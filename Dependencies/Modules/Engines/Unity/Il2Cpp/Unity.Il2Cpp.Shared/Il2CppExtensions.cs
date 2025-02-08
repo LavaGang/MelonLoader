1ab10c0c1b3de43abc92cc927dac83f7abe1ea44
Herp Derpinstine <herpderpinstine@gmail.com>
2025-02-08 03:43:29 -0600

    Reimplemented Unity Utilities in Unity.Il2Cpp.Shared

--- Dependencies/Modules/Engines/Unity/Il2Cpp/Unity.Il2Cpp.Shared/Il2CppExtensions.cs
+++ Dependencies/Modules/Engines/Unity/Il2Cpp/Unity.Il2Cpp.Shared/Il2CppExtensions.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿using Il2CppInterop.Common;
+using Il2CppInterop.Common.Attributes;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes;
+using System;
+using System.Collections;
+using System.Reflection;
 using UnityEngine;
 
 namespace MelonLoader.Engine.Unity.Il2Cpp
@@ -7,15 +14,67 @@ namespace MelonLoader.Engine.Unity.Il2Cpp
     {
         #region Coroutine
 
-        internal static Coroutine StartCoroutine<T>(this T behaviour, IEnumerator enumerator)
+        public static Coroutine StartCoroutine<T>(this T behaviour, IEnumerator enumerator)
             where T : MonoBehaviour
             => behaviour.StartCoroutine(
                 new Il2CppSystem.Collections.IEnumerator(
                 new Il2CppEnumeratorWrapper(enumerator).Pointer));
 
-        internal static void StopCoroutine<T>(this T behaviour, Coroutine routine)
+        public static void StopCoroutine<T>(this T behaviour, Coroutine routine)
             where T : MonoBehaviour
             => behaviour.StopCoroutine(routine);
+
+        #endregion
+
+        #region Interop
+
+        public static int? GetIl2CppMethodCallerCount(this MethodBase method)
+        {
+            CallerCountAttribute att = method.GetCustomAttribute<CallerCountAttribute>();
+            if (att == null)
+                return null;
+            return att.Count;
+        }
+
+        public static FieldInfo MethodBaseToIl2CppFieldInfo(this MethodBase method)
+            => Il2CppInteropUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(method);
+
+        public static void RegisterTypeInIl2CppDomain(this Type type, bool logSuccess)
+            => ClassInjector.RegisterTypeInIl2Cpp(type, new() { LogSuccess = logSuccess });
+        public static void RegisterTypeInIl2CppDomainWithInterfaces(this Type type, Type[] interfaces, bool logSuccess)
+            => ClassInjector.RegisterTypeInIl2Cpp(type, new() { LogSuccess = logSuccess, Interfaces = interfaces });
+
+        public static bool IsGeneratedAssemblyType(this Type type)
+            => type.IsInheritedFromIl2CppObjectBase() && !type.IsInjectedType();
+
+        public static bool IsInheritedFromIl2CppObjectBase(this Type type)
+            => (type != null) && type.IsSubclassOf(typeof(Il2CppObjectBase));
+
+        public static bool IsInjectedType(this Type type)
+        {
+            IntPtr ptr = GetClassPointerForType(type);
+            return ptr != IntPtr.Zero && RuntimeSpecificsStore.IsInjected(ptr);
+        }
+
+        public static IntPtr GetClassPointerForType(this Type type)
+        {
+            if (type == typeof(void)) return Il2CppClassPointerStore<Il2CppSystem.Void>.NativeClassPtr;
+            return (IntPtr)typeof(Il2CppClassPointerStore<>).MakeGenericType(type)
+                  .GetField(nameof(Il2CppClassPointerStore<int>.NativeClassPtr)).GetValue(null);
+        }
+
+        public static T Il2CppObjectPtrToIl2CppObject<T>(this IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+                throw new NullReferenceException("The ptr cannot be IntPtr.Zero.");
+            if (!IsGeneratedAssemblyType(typeof(T)))
+                throw new NullReferenceException("The type must be a Generated Assembly Type.");
+            return (T)typeof(T).GetConstructor(BindingFlags.Public | BindingFlags.Instance, 
+                null, 
+                [typeof(IntPtr)], 
+                Array.Empty<ParameterModifier>())
+                .Invoke([ptr]);
+        }
 
         #endregion
     }
